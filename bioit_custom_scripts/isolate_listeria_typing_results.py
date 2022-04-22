c8fe6cd09323f73fd264b5dfdab724d83ba16ea1
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-04-22 16:07:15 +0200

    fix some more mistakes

--- bioit_custom_scripts/isolate_listeria_typing_results.py
+++ bioit_custom_scripts/isolate_listeria_typing_results.py
@@ -69,7 +69,7 @@ cur = con.cursor()
 
 #main
 def insert_typing_results():
-    reportlink = f'<p><a href="/galaxyreports/listeria/{isolate_name}/report.html"> html report</a></p>'
+    reportlink =f'<p><a href="/galaxyreports/listeria/{isolate_name}/report.html"> html report</a></p>'
     cur.execute(f"INSERT INTO eav_text(isolate_id, "
                 f"field, value)"
                 f"VALUES((SELECT id FROM isolates WHERE isolate='{isolate_name}'),"
