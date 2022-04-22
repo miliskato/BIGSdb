21a91622bd6475664573edc9d987e18caf70a574
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-04-22 15:16:47 +0200

    fix some more mistakes

--- bioit_custom_scripts/insert_assemblies.py
+++ bioit_custom_scripts/insert_assemblies.py
@@ -60,8 +60,8 @@ if presentcontigs[0][0] == 0:
                     f"'f', '{sequence}', '{sequencename}', 1, "
                     f"1, (SELECT CURRENT_DATE),(SELECT CURRENT_DATE))")
 
-        # remove the file
-        os.remove(Path(fastafile))
+    # remove the file
+    os.remove(Path(fastafile))
 
 else:
     sys.exit(f"isolate {isolate_name} already contains assembly records!")
