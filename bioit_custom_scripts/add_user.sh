e561f349903b7aeb1538fd096f122c27654f2b94
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-04-25 17:14:50 +0200

    script to add users to all db

---
+++ bioit_custom_scripts/add_user.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+# this script should be executed as postgres user
+# arguments to be passed:
+# $1 first name
+# $2 last name
+
+# This script inserts a user into the global user database,
+# its password in bigsdb_auth,
+# and the user into the users tables of the species db
+
+firstname=$(echo ${1:0:2})
+username=$firstname$2
+password=($(echo -n 'mikelchtermans' | md5sum))
+echo $username $password
+
+perl /home/BIGSdb/scripts/maintenance/add_user.pl -a -d global_bigsdb_users -n $username -p $password
+psql -d global_bigsdb_users -c "INSERT INTO users (user_name,surname,first_name,email,affiliation,date_entered,datestamp,status) VALUES ('${username}','${2}','${1}', '${1}.${2}@sciensano.be','Sciensano','now','now', 'validated');"
+psql -d bigsdb_listeria_seqdef -c "INSERT INTO users (id, user_name, surname, first_name, email, affiliation, status, date_entered, datestamp, curator, user_db) VALUES ((SELECT(SELECT MAX(id) FROM users)+1),'${username}','${2}','${1}', '${1}.${2}@sciensano.be','Sciensano','user','now','now', 0, 1);"
+psql -d bigsdb_listeria_isolates -c "INSERT INTO users (id, user_name, surname, first_name, email, affiliation, status, date_entered, datestamp, curator, user_db) VALUES ((SELECT(SELECT MAX(id) FROM users)+1),'${username}','${2}','${1}', '${1}.${2}@sciensano.be','Sciensano','user','now','now', 0, 1);"
+psql -d bigsdb_mycobacterium_seqdef -c "INSERT INTO users (id, user_name, surname, first_name, email, affiliation, status, date_entered, datestamp, curator, user_db) VALUES ((SELECT(SELECT MAX(id) FROM users)+1),'${username}','${2}','${1}', '${1}.${2}@sciensano.be','Sciensano','user','now','now', 0, 1);"
+psql -d bigsdb_mycobacterium_isolates -c "INSERT INTO users (id, user_name, surname, first_name, email, affiliation, status, date_entered, datestamp, curator, user_db) VALUES ((SELECT(SELECT MAX(id) FROM users)+1),'${username}','${2}','${1}', '${1}.${2}@sciensano.be','Sciensano','user','now','now', 0, 1);"
+
+exit
