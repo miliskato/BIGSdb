1e979b27ec1f950900f7877a3c036d49a19e2eb5
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-05-16 13:45:35 +0200

    open report and tsv link in new window

--- bioit_custom_scripts/isolate_STEC_typing_results.py
+++ bioit_custom_scripts/isolate_STEC_typing_results.py
@@ -62,7 +62,7 @@ cur = con.cursor()
 
 #main
 def insert_typing_results():
-    reportlink =f'<p><a href="/galaxyreports/stec/{isolate_name}/report.html"> html report</a></p>'
+    reportlink =f'<p><a href="/galaxyreports/stec/{isolate_name}/report.html" target="_blank"> html report</a></p>'
     cur.execute(f"INSERT INTO eav_text(isolate_id, "
                 f"field, value)"
                 f"VALUES((SELECT id FROM isolates WHERE isolate='{isolate_name}'),"
