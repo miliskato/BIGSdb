c5ffd00e03b11600f83785317b17b55196e3d5e8
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-05-23 10:54:23 +0200

    removing cgmlst_similar_isolates for now

--- bioit_custom_scripts/cron_startinsertion.sh
+++ bioit_custom_scripts/cron_startinsertion.sh
@@ -24,7 +24,7 @@ do
     /home/BIGSdb/3.9PythonVenv/bin/python3.9 /home/BIGSdb/bioit_custom_scripts/htmltagger.py --htmlfilepath /reports/$species/$sample_name/report.html --species $species
     /home/BIGSdb/3.9PythonVenv/bin/python3.9 /home/BIGSdb/bioit_custom_scripts/isolate_${species}_typing_results.py --tsvfilepath /reports/$species/$sample_name/report.tsv --isolatename $sample_name --uploadermailadress $uploader
     /home/BIGSdb/3.9PythonVenv/bin/python3.9 /home/BIGSdb/bioit_custom_scripts/insert_assemblies.py --fastafilepath /reports/$species/$sample_name/assembly/${sample_name}_contigs.fasta --isolatename $sample_name --species $species
-    /home/BIGSdb/3.9PythonVenv/bin/python3.9 /home/BIGSdb/bioit_custom_scripts/cgmlst_similar_isolates.py --isolatename $sample_name --species $species
+##    /home/BIGSdb/3.9PythonVenv/bin/python3.9 /home/BIGSdb/bioit_custom_scripts/cgmlst_similar_isolates.py --isolatename $sample_name --species $species
   } 2>&1 | tee /home/galaxy/$sample_name.bigsdb_insertion.log
   mv /home/galaxy/$sample_name.bigsdb_insertion.log /reports/$species/$sample_name/
 done
