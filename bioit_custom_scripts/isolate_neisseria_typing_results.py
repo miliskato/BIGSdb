bac8aa3bbfaceca8e0374d04f3f32caa391ee97e
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-07-20 11:35:46 +0200

    adding the updated email traceback everywhere (forgot some)

--- bioit_custom_scripts/isolate_neisseria_typing_results.py
+++ bioit_custom_scripts/isolate_neisseria_typing_results.py
@@ -481,7 +481,7 @@ elif sample_presence[0][0] == 1:
         except Exception as exceptionmessage:
             send_email(
                 f'Error inserting output of neisseria pipeline to bigsdb for sample {isolate_name} on host {socket.gethostname()}',
-                f"{exceptionmessage}", emaildict)
+                f"{exceptionmessage}\n{traceback.format_exc()}", emaildict)
     elif sample_presence[0][0] >= 1:
         sys.exit("This sample already contains typing results")
 
--- bioit_custom_scripts/isolate_salmonella_typing_results.py
+++ bioit_custom_scripts/isolate_salmonella_typing_results.py
@@ -656,7 +656,7 @@ elif sample_presence[0][0] == 1:
         except Exception as exceptionmessage:
             send_email(
                 f'Error inserting output of salmonella pipeline to bigsdb for sample {isolate_name} on host {socket.gethostname()}',
-                f"{exceptionmessage}", emaildict)
+                f"{exceptionmessage}\n{traceback.format_exc()}", emaildict)
     elif sample_presence[0][0] >= 1:
         sys.exit("This sample already contains typing results")
 
