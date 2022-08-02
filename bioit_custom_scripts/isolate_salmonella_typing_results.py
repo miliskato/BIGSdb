3a5b2565f9a4fc4cf453eadf6ef3c005c8ec041e
Benoît <benoit.bergkpinto@sciensano.be>
2022-08-02 14:32:41 +0200

    fixed seqsero and spifinder to be compatible with the new salmonella pipeline.

--- bioit_custom_scripts/isolate_salmonella_typing_results.py
+++ bioit_custom_scripts/isolate_salmonella_typing_results.py
@@ -13,7 +13,8 @@ import traceback
 
 schemedict = {'salmonella_mlst': {'dirdb': '/db/sequence_typing/salmonella/mlst', 'tsvname': 'mlst'},
               'salmonella_cgmlst': {'dirdb': '/db/sequence_typing/salmonella/cgmlst', 'tsvname': 'cgmlst'},
-              'salmonella_pointfinder': {'dirdb': '', 'tsvname': 'pointfinder_mutations', 'schemename_html': 'PointFinder'},
+              'salmonella_pointfinder': {'dirdb': '', 'tsvname': 'pointfinder_mutations',
+                                         'schemename_html': 'PointFinder'},
               'salmonella_genotyphi': {'dirdb': '', 'tsvname': 'genotyphi'},
               'salmonella_serotyping': {'dirdb': '', 'tsvname': 'serotyping'},
               'salmonella_spifinder': {'dirdb': '', 'tsvname': 'spifinder'}
@@ -45,8 +46,8 @@ genedetectiondict = {'salmonella_ndaro':
                      }
 
 emaildict = {"from": "bioit-dev1@wiv-isp.be",
-    "to": "michael.kelchtermans@sciensano.be, benoit.bergkpinto@sciensano.be",
-    "host": "smtp.wiv-isp.be"}
+             "to": "michael.kelchtermans@sciensano.be, benoit.bergkpinto@sciensano.be",
+             "host": "smtp.wiv-isp.be"}
 
 isolatedb = 'bigsdb_salmonella_isolates'
 seqdefdb = 'bigsdb_salmonella_seqdef'
@@ -57,7 +58,7 @@ argument_parser.add_argument('--uploadermailadress', required=True, type=str)
 args = argument_parser.parse_args()
 tsvfilepath = Path(args.tsvfilepath)
 isolate_name = args.isolatename
-uploadermailadress=args.uploadermailadress
+uploadermailadress = args.uploadermailadress
 
 # todo change curator/sender to NRC (all the 1s in inserts and updates)
 
@@ -73,7 +74,7 @@ con.autocommit = True
 cur = con.cursor()
 
 
-#main
+# main
 def insert_typing_results():
     con = psycopg2.connect(database=f"{isolatedb}", user='apache', password='remote',
                            host='127.0.0.1', port='')
@@ -83,7 +84,7 @@ def insert_typing_results():
                             host='127.0.0.1', port='')
     cur2 = con2.cursor()
     con2.autocommit = True
-    reportlink =f'<p><a href="/galaxyreports/salmonella/{isolate_name}/report.html" target="_blank"> html report</a></p>'
+    reportlink = f'<p><a href="/galaxyreports/salmonella/{isolate_name}/report.html" target="_blank"> html report</a></p>'
     cur.execute(f"INSERT INTO eav_text(isolate_id, "
                 f"field, value)"
                 f"VALUES((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),"
@@ -92,14 +93,14 @@ def insert_typing_results():
                 f"field, value)"
                 f"VALUES((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),"
                 f"'tsv', '{reportlink.replace('html', 'tsv')}') ")
-    dirlist = [] #dirlist serves to not insert duplicates (creates error in sql), for salmonella e.g. prs and prfA are included in two schemes
+    dirlist = []  # dirlist serves to not insert duplicates (creates error in sql), for salmonella e.g. prs and prfA are included in two schemes
     for scheme in schemedict:
         if schemedict[scheme]['dirdb'] != '':
             dirs = next(os.walk(schemedict[scheme]['dirdb']))[1]
             for dir in dirs:
                 if not dir.startswith('.') and dir not in dirlist:
                     dirlist.append(dir)
-                    result = outputtsvdict['-'.join([schemedict[scheme]['tsvname'],dir])].split(',')
+                    result = outputtsvdict['-'.join([schemedict[scheme]['tsvname'], dir])].split(',')
                     if result[2] == '100.00' and result[3] != '-' and eval(result[3]) == 1.0:
                         allele_id = int(result[1])
                         cur.execute(f"INSERT INTO allele_designations(locus, isolate_id, "
@@ -126,17 +127,20 @@ def insert_typing_results():
                             # sometimes a mutation will give resistance to more than 1 AB
                             antibiotics = hit[3].split(',')
                             for antibiotic in antibiotics:
-                                antibiotic_reformatted = '_'.join(['POINTFINDER', antibiotic.replace('-', '_').replace(' ', '_').upper()])
+                                antibiotic_reformatted = '_'.join(
+                                    ['POINTFINDER', antibiotic.replace('-', '_').replace(' ', '_').upper()])
                                 mutation = hit[0].replace('.', '_').replace(' ', '_')
                                 eavhtmltable = eavhtmltable + ''.join(
                                     ['<tr><td><a href="/galaxyreports/salmonella/', isolate_name, '/report.html#',
                                      schemedict[scheme]['schemename_html'], '" target="_blank">',
                                      hit[0], '</a></td>'])
                                 eavhtmltable = eavhtmltable + ''.join(['<td>', antibiotic, '</td></tr>'])
-                                cur2.execute(f"SELECT allele_id FROM sequences WHERE allele_id = '{mutation}' and locus = '{antibiotic_reformatted}'")
+                                cur2.execute(
+                                    f"SELECT allele_id FROM sequences WHERE allele_id = '{mutation}' and locus = '{antibiotic_reformatted}'")
                                 present = cur2.fetchall()
                                 if present == []:
-                                    cur2.execute(f"SELECT sequence FROM sequences WHERE locus  ='{antibiotic_reformatted}' ORDER BY CHAR_LENGTH(sequence) DESC LIMIT 1")
+                                    cur2.execute(
+                                        f"SELECT sequence FROM sequences WHERE locus  ='{antibiotic_reformatted}' ORDER BY CHAR_LENGTH(sequence) DESC LIMIT 1")
                                     longest_dummy_sequence = cur2.fetchall()
                                     if longest_dummy_sequence == []:
                                         dummysequence = 'TAG'
@@ -197,20 +201,22 @@ def insert_typing_results():
                 cur.execute(f"SELECT field FROM eav_fields WHERE category = 'Serotyping' ")
                 serotyping_list = cur.fetchall()
                 for sero in serotyping_list:
-                    tool = re.sub('_[a-z]*$','', sero[0])
-                    field_type = sero[0].split('_')[len(sero[0].split('_'))-1]
+                    tool = re.sub('_[a-z]*$', '', sero[0])
+                    field_type = sero[0].split('_')[len(sero[0].split('_')) - 1]
+
                     # class for serotyping formula processing.
                     class formula:
-                        def __init__(self,rawFormula,tool,isolate_name):
-                            self.antigens={ "O_antigen" : rawFormula.split(':')[0].split(','),
-                                            "H1_antigen" : rawFormula.split(':')[1].split(','),
-                                             "H2_antigen" : rawFormula.split(':')[2].split(',')}
+                        def __init__(self, rawFormula, tool, isolate_name):
+                            self.antigens = {"O_antigen": rawFormula.split(':')[0].split(','),
+                                             "H1_antigen": rawFormula.split(':')[1].split(','),
+                                             "H2_antigen": rawFormula.split(':')[2].split(',')}
                             self.tool = tool
                             self.isolate_name = isolate_name
+
                         def __check_if_exist_in_seqdef(self, field, entry):
                             cur2.execute(
                                 f"SELECT allele_id FROM sequences WHERE allele_id = '{entry}' and locus = '{field}'")
-                            return(cur2.fetchall())
+                            return (cur2.fetchall())
 
                         def __generate_dummy_sequence(self, field):
                             cur2.execute(
@@ -223,7 +229,7 @@ def insert_typing_results():
                             return dummysequence
 
                         def insert_antigens_into_db(self):
-                            antigens=["O_antigen","H1_antigen" ,"H2_antigen"]
+                            antigens = ["O_antigen", "H1_antigen", "H2_antigen"]
                             for antigen in antigens:
                                 field = (f'{self.tool}_{antigen}').upper()
                                 entries = self.antigens[antigen]
@@ -249,37 +255,40 @@ def insert_typing_results():
                                     sistr_formula = formula(serotypingInsert, tool, isolate_name)
                                     sistr_formula.insert_antigens_into_db()
                                     cur.execute(
-                                    f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
+                                        f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
                         elif field_type == 'serotype':
                             if f'{tool}_serotype_concensus' in outputtsvdict:
                                 serotypingInsert = outputtsvdict[f'{tool}_serotype_concensus']
                                 if serotypingInsert != '-':
                                     cur.execute(
-                                    f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
+                                        f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
                     else:
-                        if field_type == 'formula' and f'{tool} Predicted antigenic profile:' in outputtsvdict:
-                            serotypingInsert = outputtsvdict[f'{tool} Predicted antigenic profile:']
+                        if field_type == 'formula' and f'{tool}_Predicted_antigenic_profile' in outputtsvdict:
+                            serotypingInsert = outputtsvdict[f'{tool}_Predicted_antigenic_profile']
                             seqsero_formula = formula(serotypingInsert, tool, isolate_name)
                             seqsero_formula.insert_antigens_into_db()
                             if serotypingInsert != '-:-:-':
                                 cur.execute(
-                                f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
-                        elif field_type == 'serotype' and f'{tool} Predicted serotype:' in outputtsvdict:
-                            serotypingInsert = outputtsvdict[f'{tool} Predicted serotype:']
+                                    f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
+                        elif field_type == 'serotype' and f'{tool}_Predicted_serotype' in outputtsvdict:
+                            serotypingInsert = outputtsvdict[f'{tool}_Predicted_serotype']
                             if serotypingInsert != '- -:-:-':
                                 cur.execute(
-                                f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
+                                    f"INSERT INTO eav_text(isolate_id, field, value) VALUES ((SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'),'{sero[0]}','{serotypingInsert}')")
 
             elif scheme == 'salmonella_spifinder':
-                schemes_spifinder=['spifinder_fastq','spifinder_fasta']
+                schemes_spifinder = ['spifinder_fastq', 'spifinder_fasta']
                 for scheme in schemes_spifinder:
                     if 'spifinder_fastq' in outputtsvdict:
                         hits = outputtsvdict[scheme]
                         if hits != '[]':
                             hits = ast.literal_eval(hits)
                             for l in range(0, len(hits)):
-                                spifinder_entry = f"CatFunc{hits[l]['category_function']}__{hits[l]['accession']}"
-                                spifinder_field = (f"{scheme}_{hits[l]['SPI']}").upper()
+                                if scheme == 'spifinder_fastq':
+                                    spifinder_entry = f"CatFunc{hits[l][6]}__{hits[l][4]}"
+                                else:
+                                    spifinder_entry = f"CatFunc{hits[l][8]}__{hits[l][6]}"
+                                spifinder_field = f"{scheme}_{hits[l][0]}".upper()
                                 cur2.execute(
                                     f"SELECT allele_id FROM sequences WHERE allele_id = '{spifinder_entry}' and locus = '{spifinder_field}'")
                                 present_spifinder = cur2.fetchall()
@@ -299,14 +308,13 @@ def insert_typing_results():
                                 presence_allele_designation = cur.fetchall()
                                 if presence_allele_designation == []:
                                     cur.execute(f"INSERT INTO allele_designations(locus, isolate_id, "
-                                            f"allele_id, status, method, sender, "
-                                            f"curator, date_entered, datestamp) "
-                                            f"VALUES('{spifinder_field}', (SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'), "
-                                            f"'{spifinder_entry}', 'confirmed', 'automatic', 1, "
-                                            f"1, (SELECT CURRENT_DATE),(SELECT CURRENT_DATE))")
+                                                f"allele_id, status, method, sender, "
+                                                f"curator, date_entered, datestamp) "
+                                                f"VALUES('{spifinder_field}', (SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}'), "
+                                                f"'{spifinder_entry}', 'confirmed', 'automatic', 1, "
+                                                f"1, (SELECT CURRENT_DATE),(SELECT CURRENT_DATE))")
 
-
-                con2.close() #close seqdef database
+                con2.close()  # close seqdef database
 
     cur.execute(f"INSERT INTO history(isolate_id, timestamp, action, curator)"
                 f"VALUES((SELECT MAX(id) FROM isolates WHERE isolate = '{isolate_name}'),(SELECT NOW()::TIMESTAMP), 'Typing results inserted', 1)")
@@ -613,6 +621,7 @@ def insert_typing_results():
     cur.execute(f"INSERT INTO history(isolate_id, timestamp, action, curator)"
                 f"VALUES((SELECT MAX(id) FROM isolates WHERE isolate = '{isolate_name}'),(SELECT NOW()::TIMESTAMP), 'Gene detection results inserted', 1)")
 
+
 def send_email(subject: str, content: str, config: dict) -> None:
     """
     Sends an email.
@@ -627,6 +636,7 @@ def send_email(subject: str, content: str, config: dict) -> None:
     message.set_content(content)
     with smtplib.SMTP(config['host']) as s:
         s.send_message(message)
+
 
 # check whether sample exists
 cur.execute(f"SELECT COUNT(*) FROM isolates WHERE isolate='{isolate_name}'")
@@ -648,7 +658,8 @@ if sample_presence[0][0] == 0:
 
 elif sample_presence[0][0] == 1:
     # sample exists: check whether typing results or not (we do not bother checking for all schemes separately
-    cur.execute(f"SELECT COUNT(*) FROM allele_designations WHERE isolate_id = (SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}')")
+    cur.execute(
+        f"SELECT COUNT(*) FROM allele_designations WHERE isolate_id = (SELECT MAX(id) FROM isolates WHERE isolate='{isolate_name}')")
     alleles_presence = cur.fetchall()
     if alleles_presence[0][0] == 0:
         # no allele designations are present so we insert them
@@ -678,5 +689,5 @@ elif sample_presence[0][0] >= 1:
     elif sample_presence[0][0] >= 1:
         sys.exit("This sample already contains typing results")
 
-#close db connection
+# close db connection
 con.close()
