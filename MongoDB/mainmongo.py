545c0d50b2a50c1ff20e60b8ec0d0e72bf081659
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-10-13 10:37:07 +0200

    Adding allele sequences to new_alleles collection and scrubbing them from the documents to be inserted in the results collections

--- MongoDB/mainmongo.py
+++ MongoDB/mainmongo.py
@@ -104,7 +104,7 @@ def find_hashes_in_results_and_add_to_collection(results: dict, mongoinit: objec
     hashed_AD_collection = mongoinit.initialise_hashing_collection(config_data, species)
     for typing_scheme in ['mlst', 'cgmlst']:
         if typing_scheme in results.keys():
-            for allele_info in results[typing_scheme]['loci']:
+            for locus_index, allele_info in enumerate(results[typing_scheme]['loci']):
                 # check if allele designation is md5 hash (32 char combination of letters andor numbers)
                 if re.findall(r'(?i)(?<![a-z0-9])[a-f0-9]{32}(?![a-z0-9])', allele_info['Allele']):
                     existing_document = hashed_AD_collection.with_options(read_concern=ReadConcern(level="majority")).find_one({"scheme": typing_scheme, "locus": allele_info['Locus'], "hashed_allele": allele_info['Allele']})
@@ -112,6 +112,7 @@ def find_hashes_in_results_and_add_to_collection(results: dict, mongoinit: objec
                         _write_document(hashed_AD_collection, {"scheme": typing_scheme,
                                                                "locus": allele_info['Locus'],
                                                                "hashed_allele": allele_info['Allele'],
+                                                               "allele_sequence" : allele_info['Allele_sequence'],
                                                                "encountered_count": 1,
                                                                "resolved_AD": 0
                                                                })
@@ -119,6 +120,7 @@ def find_hashes_in_results_and_add_to_collection(results: dict, mongoinit: objec
                         hashed_AD_collection.with_options(write_concern=WriteConcern(w="majority")).update_one({"_id": existing_document['_id']},
                                                                                                                {"$inc": {"encountered_count": 1}})
                         logging.info(f"hashed allele '{allele_info['Allele']}' encounter incremented by one")
+                    results[typing_scheme]['loci'][locus_index].pop('Allele_sequence')
 
 def _return_YMD_from_YMDhms(datetimestring: str):
     return datetime.datetime.strptime(datetimestring, '%d/%m/%Y - %X').strftime('%Y-%m-%d')
@@ -162,10 +164,10 @@ if __name__ == '__main__':
                     raise RuntimeError('No qc values found in the given results')
 
                 if sample_quality == 'good':
+                    find_hashes_in_results_and_add_to_collection(records, mongoinit, config_data, args.species)
                     _write_document(isolates_collection, _new_isolate(args.technical_id, args.vcffilepath, args.fastafilepath,
                                                                       records))
                     logging.info(f"Wrote new isolate {args.technical_id} and its result to {args.species} database")
-                    find_hashes_in_results_and_add_to_collection(records, mongoinit, config_data, args.species)
                 else:
                     _write_document(isolates_badqc_collection, _new_isolate(args.technical_id, args.vcffilepath, args.fastafilepath,
                                                                       records))
