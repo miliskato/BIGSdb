861f8d9fd410ed6542d278c95d6236b62b915913
mikelchtermans <michael.kelchtermans@sciensano.be>
2022-10-18 17:00:51 +0200

    Adding assembly and vcf files to bigs

--- bioit_custom_scripts/upgrade_isolates.sql
+++ bioit_custom_scripts/upgrade_isolates.sql
@@ -1,10 +1,3 @@
-CREATE TABLE eav_fields_hidden AS (SELECT * FROM eav_fields) WITH NO DATA;
-ALTER TABLE eav_fields_hidden ADD PRIMARY KEY(field);
-GRANT SELECT, INSERT, UPDATE, DELETE ON eav_fields_hidden TO apache;
-INSERT INTO eav_fields_hidden(field, value_format, description, no_curate, no_submissions, datestamp, curator) VALUES('hash_fastq_md5_forward', 'text', '', 't', 't', (SELECT CURRENT_DATE), 1);
-INSERT INTO eav_fields_hidden(field, value_format, description, no_curate, no_submissions, datestamp, curator) VALUES('hash_fastq_md5_reverse', 'text', '', 't', 't', (SELECT CURRENT_DATE), 1);
-INSERT INTO eav_fields_hidden(field, value_format, description, no_curate, no_submissions, datestamp, curator) VALUES('hash_fasta_md5', 'text', '', 't', 't', (SELECT CURRENT_DATE), 1);
-INSERT INTO eav_fields_hidden(field, value_format, description, no_curate, no_submissions, datestamp, curator) VALUES('mongo_results_version', 'text', '', 't', 't', (SELECT CURRENT_DATE), 1);
-DELETE FROM eav_fields WHERE field LIKE 'hash_fastq_md5_%';
-ALTER TABLE isolates ADD latest_analysis_date date;
-UPDATE isolates SET latest_analysis_date='2022-08-01';
+INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('assembly', 'text', 'galaxy report', '', 't', 't', (SELECT CURRENT_DATE), 1);
+INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('VCF_unfiltered', 'text', 'galaxy report', '', 't', 't', (SELECT CURRENT_DATE), 1);
+INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('VCF_filtered', 'text', 'galaxy report', '', 't', 't', (SELECT CURRENT_DATE), 1);
