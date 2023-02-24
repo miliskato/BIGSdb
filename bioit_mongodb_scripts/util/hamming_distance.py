78e5be7038ae79b197cf1258689ac8bfbb3bdd29
mikelchtermans <michael.kelchtermans@sciensano.be>
2023-02-24 15:24:11 +0100

    remove type() 2

--- bioit_mongodb_scripts/util/hamming_distance.py
+++ bioit_mongodb_scripts/util/hamming_distance.py
@@ -9,7 +9,7 @@ from multiprocessing import Pool
 from bioit_mongodb_scripts.config import MONGO_CONFIG
 
 
-def getDistance(data: np.array, func_name: str, pool: Pool, start=0) -> np.array:
+def getDistance(data: np.array, func_name: str, pool: Pool, start: int=0) -> np.array:
     """
     Main function to call to compute the hamming distance in parallel and return the half matrix
     :param data: the array containing all the cgmlst profiles to compute the distances on
@@ -38,7 +38,7 @@ def getDistance(data: np.array, func_name: str, pool: Pool, start=0) -> np.array
 
 
 
-def __parallel_dist(mat_buf: str, func: Callable, dist_buf:str, mat_shape: tuple, pool: type(Pool), start:int =0) -> None:
+def __parallel_dist(mat_buf: str, func: Callable, dist_buf:str, mat_shape: tuple, pool: Pool, start: int=0) -> None:
     """
     This function take as input a matrix stored in the buffer and containing the cgmlst profiles. This function
     distribute jobs on the pool of threads to start the computation in parallel.
