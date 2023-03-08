4b0415c973ecb11e3ef59d86d8de8c6166db9488
mikelchtermans <michael.kelchtermans@sciensano.be>
2023-03-08 14:55:52 +0100

    Only update the cache of the cgmlst scheme

--- bioit_mongodb_scripts/mongo_to_bigs.py
+++ bioit_mongodb_scripts/mongo_to_bigs.py
@@ -88,7 +88,7 @@ class MongoToBigs:
 
         # update the bigsdb cache so the clustering schemes get updated
         cache_command = f'/home/bigsdb/BIGSdb/scripts/maintenance/update_scheme_caches.pl ' \
-                  f'--database bigsdb_{self._species}_isolates'
+                  f'--database bigsdb_{self._species}_isolates --schemes 2'
         command = Command(cache_command)
         command.run(Path(os.getcwd()))
         if command.returncode != 0:
