02482655a5ecf31b6b53acf7861e5546414b10b0
mikelchtermans <michael.kelchtermans@sciensano.be>
2023-05-23 16:00:53 +0200

    Update project membership when creating a new version of an isolate

--- bioit_bigsdb_scripts/components/psql/__init__.py
+++ bioit_bigsdb_scripts/components/psql/__init__.py
@@ -18,6 +18,7 @@ from .psql_tbl_locus_descriptions import TblLocusDescriptions
 from .psql_tbl_profile_fields import TblProfileFields
 from .psql_tbl_profile_members import TblProfileMembers
 from .psql_tbl_profiles import TblProfiles
+from .psql_tbl_project_members import TblProjectMembers
 from .psql_tbl_scheme_members import TblSchemeMembers
 from .psql_tbl_seq_bin_stats import TblSeqBinStats
 from .psql_tbl_sequence_bin import TblSequenceBin
