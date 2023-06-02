ed1a4e6654bb041102a656c56605d8477a7e5247
AnGo5687 <ann-stephan.gori@sciensano.be>
2023-06-02 11:50:47 +0200

    test date handling

---
+++ bioit_bigsdb_scripts/infile_tool/insert_json_labdata.py
@@ -0,0 +1,112 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+from typing import List
+
+import numpy as np
+import pandas as pd
+import re
+
+from pandas.errors import ParserError
+
+PYTHONPATH = Path(__file__).resolve().parent.parent
+sys.path.append(str(PYTHONPATH))
+
+from bioit_bigsdb_scripts.components.psql import TblIsolates
+from bioit_bigsdb_scripts.components.python_utility_functions import get_bigsdb_config_data
+
+
+def parse_arguments(specieslist: List[str]) -> argparse.Namespace:
+    """
+    Parses the command line arguments.
+    :param specieslist: list of all the species choices
+    :return: Parsed arguments
+    """
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument('--jsonfilepath', required=True, type=Path)
+    argument_parser.add_argument('--species', required=True, type=str, choices=specieslist)
+    return argument_parser.parse_args()
+
+
+def safe_date_parse(value):
+    try:
+        return pd.to_datetime(value, format='%Y-%m-%d', errors='coerce')
+    except (ParserError, ValueError):
+        try:
+            return pd.to_datetime(value, format='%Y-%m-%d %H:%M:%S', errors='coerce')
+        except (ParserError, ValueError):
+            try:
+                return pd.to_datetime(value, format='%d/%m/%Y', errors='coerce')
+            except (ParserError, ValueError):
+                return value
+
+
+def insert_json_labdata(species: str, jsonfilepath: Path) -> None:
+    """
+    Inserts an assembly for a given sample in bigsdb
+    :param species: commonly used bioit species name: either genus or specific like stec
+    :param fastafilepath: path of the fasta file
+    :return: None
+    """
+
+df = pd.read_json(jsonfilepath)
+col3name = df.columns.values[2]
+col6name = df.columns.values[5]
+col10name = df.columns.values[9]
+col13name = df.columns.values[12]  # sex
+col14name = df.columns.values[13]  # zipcode
+
+#date_form_regex = ['\d+/\d+/\d+','\d+-\d+-\d+','\d+-\d+-\d+\s\d+-\d+-\d+']
+for item in df.columns.values:
+    df[item] = df[item].apply(safe_date_parse)
+
+#(df[col10name].str.match('\d+/\d+/\d+'))
+df[col3name] = df[col3name].apply(lambda x: pd.to_datetime(x, format='%Y-%m-%d', errors='coerce'))
+df[col6name] = df[col6name].apply(lambda x: pd.to_datetime(x, format='%Y-%m-%d %H:%M:%S', errors='coerce'))
+df[col10name] = df[col10name].apply(lambda x: pd.to_datetime(x, format='%d/%m/%Y', errors='coerce'))
+
+df[col13name].replace(0, np.nan, inplace=True)
+df[col14name].replace(0, np.nan, inplace=True)
+
+config = get_bigsdb_config_data()
+db_metadata_mappings = config['lab_meta_data']
+
+metadata_map_species = db_metadata_mappings[species]
+
+# prepare query
+species_update_query = TblIsolates.build_update_nomin_metadata_query(metadata_map_species)
+
+tbl_isolates = TblIsolates(species)
+# update rows
+df = df.reset_index()
+fail = 0
+
+for index, row in df.iterrows():
+    try:
+        params = []
+        for key in metadata_map_species:
+            if pd.isnull(row[metadata_map_species[key]]) or row[metadata_map_species[key]] == ' ':
+                params.append(None)
+            else:
+                params.append(row[metadata_map_species[key]])
+        params.append(row['id'])
+        tbl_isolates.update_nomin_metadata(species_update_query, params)
+    except Exception as e:
+        fail += 1
+        print(str.format("Error on id {} (Skipping it): {}", row['id'], e));
+
+print(str.format("Updated {} items, {} failures", (df.shape[0]-fail), fail))
+
+if __name__ == '__main__':
+    # Configure stdout logging
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
+    # Get Bigsdb config
+    bigsdb_config_data = get_bigsdb_config_data()
+
+    # Parse arguments
+    args = parse_arguments(list(bigsdb_config_data['lab_meta_data']))
+
+    # run main
+    insert_json_labdata(args.species, args.jsonfilepath)
