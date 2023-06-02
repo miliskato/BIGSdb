a8d4203b56193b6e374a4b560de3fc2fdf13dfb1
AnGo5687 <ann-stephan.gori@sciensano.be>
2023-06-02 11:48:27 +0200

    clean install gt pip

--- bioit_bigsdb_scripts/infile_tool/excel_to_json.py
+++ bioit_bigsdb_scripts/infile_tool/excel_to_json.py
@@ -1,18 +1,13 @@
 import pandas as pd
 
-#PYTHONPATH
+df = pd.read_excel('/home/angori/PycharmProject/DeployTest/BIGSdb_Neisseria_metadata_BioIT.xlsx',engine='openpyxl')
 
-df_data = pd.read_excel('/home/angori/PycharmProject/DeployTest/BIGSdb_Neisseria_metadata_BioIT.xlsx',engine='openpyxl')
-
-col3name = df.columns.values[2]
-col6name = df.columns.values[5]
-col10name = df.columns.values[9]
-
-df[col3name] = df[col3name].apply(lambda x: pd.to_datetime (x, format='%Y-%m-%d', errors='coerce'))
-df[col6name] = df[col6name].apply(lambda x: pd.to_datetime (x, format='%Y-%m-%d %H:%M:%S', errors='coerce'))
-df[col10name] = df[col10name].apply(lambda x: pd.to_datetime (x, format='%d/%m/%Y',errors='coerce'))
-
+df=df.loc[df['uploader'] != 'jolein.laumen@sciensano.be']
+df=df.loc[df['id'] != 226]
+df=df.loc[df['id'] != 154]
 shuff_df = df.apply(lambda x: x.sample(frac=1).values)
+shuff_df.to_json('/home/angori/PycharmProject/DeployTest/BIGSdb_Neisseria_metadata_BioITshuff.json')
 
-json_str=df.to_json(date_format='iso')
+for col in df:
+    print(df[col].unique())
 
