34e906501128ae79561d8c684665132ea97d511b
mikelchtermans <michael.kelchtermans@sciensano.be>
2023-06-16 12:32:55 +0200

    Remove lower() because fasta filepaths are not lower case since new db update framework started beginning of June

--- bioit_mongodb_scripts/tempid_replacer.py
+++ bioit_mongodb_scripts/tempid_replacer.py
@@ -144,10 +144,10 @@ class TempidReplacer:
         """
         if self._species == 'stec':
             fasta_file = Path(
-                f"/db/sequence_typing/ecoli/{self._scheme.replace('-', '_')}/{locus}/{locus.lower()}.fasta")
+                f"/db/sequence_typing/ecoli/{self._scheme.replace('-', '_')}/{locus}/{locus}.fasta")
         else:
             fasta_file = Path(
-                f"/db/sequence_typing/{self._species}/{self._scheme.replace('-', '_')}/{locus}/{locus.lower()}.fasta")
+                f"/db/sequence_typing/{self._species}/{self._scheme.replace('-', '_')}/{locus}/{locus}.fasta")
         if fasta_file.is_file():
             logging.info(f"opening fasta file: {fasta_file}")
         else:
