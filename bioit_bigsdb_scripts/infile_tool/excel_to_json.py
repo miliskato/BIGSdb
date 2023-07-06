5bae8175f790a51b5f5c081e0f6a6ef90e16e101
AnGo5687 <ann-stephan.gori@sciensano.be>
2023-07-06 12:29:50 +0200

    remove unused lines

--- bioit_bigsdb_scripts/infile_tool/excel_to_json.py
+++ bioit_bigsdb_scripts/infile_tool/excel_to_json.py
@@ -5,6 +5,5 @@ df = pd.read_excel('/home/angori/BIGSdb_Neisseria_metadata_BioIT_26062023.xlsx',
 #shuff_df = df.apply(lambda x: x.sample(frac=1).values)
 shuff_df.to_json('/home/angori/BIGSdb_Neisseria_metadata_BioITshuff.json')
 
-for col in df:
-    print(df[col].unique())
+
 
