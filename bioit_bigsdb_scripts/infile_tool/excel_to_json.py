dd9634ed2be42fa79707cea9978d34ea44c36690
AnGo5687 <ann-stephan.gori@sciensano.be>
2023-07-13 17:03:39 +0200

    comment shuffle

--- bioit_bigsdb_scripts/infile_tool/excel_to_json.py
+++ bioit_bigsdb_scripts/infile_tool/excel_to_json.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_excel('/home/angori/BIGSdb_Neisseria_metadata_BioIT_26062023.xlsx', engine='openpyxl')
 
 #shuff_df = df.apply(lambda x: x.sample(frac=1).values)
-shuff_df.to_json('/home/angori/BIGSdb_Neisseria_metadata_BioITshuff.json')
+df.to_json('/home/angori/BIGSdb_Neisseria_metadata_BioITshuff.json')
 
 
 
