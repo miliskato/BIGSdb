1c252edf214db3af1908aa09f63e7a6f485f6a32
angori <ann-stephan.gori@sciensano.be>
2023-08-25 11:03:55 +0200

    clean the fix for upload = good quality

--- bioit_mongodb_scripts/mainmongo.py
+++ bioit_mongodb_scripts/mainmongo.py
@@ -216,7 +216,8 @@ class MainMongo:
                     for key in new_records['qc'][qc_type]:
                         if key.endswith('status') and new_records['qc'][qc_type][key] == 'Failed':
                             #good_sample_quality = False
-                            good_sample_quality = True
+                            continue  # temp fix: on NRC platform, once a sample is uploaded is considered of good quality.
+
             except KeyError:
                 send_email(
                     f"No qc values found in the given results for {self._technical_id}\n{traceback.format_exc()}",
