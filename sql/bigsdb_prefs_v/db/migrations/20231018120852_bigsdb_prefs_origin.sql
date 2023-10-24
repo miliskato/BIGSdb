5bcdd25c2c8c48bd187bf12300a7c2ccd2e4b32d
AnGo5687 <ann-stephan.gori@sciensano.be>
2023-10-24 10:11:59 +0200

    DB migration scripts for dbmate

---
+++ sql/bigsdb_prefs_v/db/migrations/20231018120852_bigsdb_prefs_origin.sql
@@ -0,0 +1,158 @@
+-- migrate:up
+CREATE TABLE guid (
+guid text NOT NULL,
+last_accessed date NOT NULL,
+PRIMARY KEY (guid)
+);
+
+CREATE TABLE general (
+guid text NOT NULL,
+dbase text NOT NULL,
+attribute text NOT NULL,
+value text NOT NULL,
+PRIMARY KEY (guid,dbase,attribute),
+CONSTRAINT conguid FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+CREATE TABLE locus (
+guid text NOT NULL,
+dbase text NOT NULL,
+locus text NOT NULL,
+action text NOT NULL,
+value text NOT NULL,
+PRIMARY KEY (guid,dbase,locus,action),
+CONSTRAINT conguid FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+CREATE TABLE scheme (
+guid text NOT NULL,
+dbase text NOT NULL,
+scheme_id int NOT NULL,
+action text NOT NULL,
+value boolean NOT NULL,
+PRIMARY KEY (guid,dbase,scheme_id,action),
+CONSTRAINT conguid FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+CREATE TABLE scheme_field (
+guid text NOT NULL,
+dbase text NOT NULL,
+scheme_id int NOT NULL,
+field text NOT NULL,
+action text NOT NULL,
+value boolean NOT NULL,
+PRIMARY KEY (guid,dbase,scheme_id,field,action),
+CONSTRAINT conguid FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+CREATE TABLE field (
+guid text NOT NULL,
+dbase text NOT NULL,
+field text NOT NULL,
+action text NOT NULL,
+value boolean NOT NULL,
+PRIMARY KEY (guid,dbase,field,action),
+CONSTRAINT conguid FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+CREATE TABLE plugin (
+guid text NOT NULL,
+dbase text NOT NULL,
+plugin text NOT NULL,
+attribute text NOT NULL,
+value text NOT NULL,
+PRIMARY KEY (guid,dbase,plugin,attribute),
+CONSTRAINT conguid FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON guid,general,locus,scheme,scheme_field,field,plugin TO apache;
+
+CREATE TABLE dashboards (
+id bigserial NOT NULL UNIQUE,
+guid text NOT NULL,
+dbase_config text NOT NULL,
+name text NOT NULL,
+data jsonb NOT NULL,
+UNIQUE (guid,dbase_config,name),
+PRIMARY KEY (id),
+FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON dashboards TO apache;
+GRANT USAGE,SELECT ON SEQUENCE dashboards_id_seq TO apache;
+
+CREATE OR REPLACE FUNCTION next_dashboard(_guid text,_dbase_config text) RETURNS text AS $next_dashboard$
+DECLARE
+		dashboard_count integer;
+BEGIN
+	SELECT COUNT(*) INTO dashboard_count FROM dashboards WHERE (guid,dbase_config)=(_guid,_dbase_config);
+	RETURN 'dashboard#' || (dashboard_count+1);
+END
+$next_dashboard$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION name_dashboard() RETURNS TRIGGER AS $next_dashboard$
+DECLARE
+	dashboard_count integer;
+BEGIN
+    IF NEW.name IS NULL THEN
+    	SELECT COUNT(*) INTO dashboard_count FROM dashboards WHERE (guid,dbase_config)=(NEW.guid,NEW.dbase_config);
+    	NEW.name := 'dashboard#' || LPAD((dashboard_count+1)::text,2,'0');
+    END IF;
+    RETURN NEW;
+END;
+$next_dashboard$ language plpgsql;
+
+CREATE TRIGGER trig_insert_dashboards
+BEFORE INSERT
+ON dashboards
+FOR EACH ROW
+EXECUTE PROCEDURE name_dashboard();
+
+CREATE TABLE active_dashboards (
+guid text NOT NULL,
+dbase_config text NOT NULL,
+id bigint NOT NULL,
+type text NOT NULL,
+value text NOT NULL,
+PRIMARY KEY(guid,dbase_config,type,value),
+FOREIGN KEY (id) REFERENCES dashboards
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON active_dashboards TO apache;
+
+CREATE TABLE dashboard_switches (
+guid text NOT NULL,
+dbase_config text NOT NULL,
+attribute text NOT NULL,
+value boolean NOT NULL,
+PRIMARY KEY (guid,dbase_config,attribute),
+FOREIGN KEY (guid) REFERENCES guid
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON dashboard_switches TO apache;
+
+
+
+-- migrate:down
+
---
+++ sql/bigsdb_rest_v/db/migrations/20231018120952_bigsdb_rest_origin.sql
@@ -0,0 +1,44 @@
+-- migrate:up
+CREATE TABLE groups (
+	name text NOT NULL,
+	description text NOT NULL,
+	long_description text,
+	PRIMARY KEY (name)
+);
+
+CREATE TABLE resources (
+	dbase_config text NOT NULL,
+	description text NOT NULL,
+	hide boolean NOT NULL DEFAULT FALSE,
+	PRIMARY KEY (dbase_config)
+);
+
+CREATE TABLE group_resources (
+	group_name text NOT NULL,
+	dbase_config text NOT NULL,
+	PRIMARY KEY (group_name,dbase_config),
+	CONSTRAINT gr_group_name FOREIGN KEY (group_name) REFERENCES groups
+	ON DELETE NO ACTION
+	ON UPDATE CASCADE,
+	CONSTRAINT gr_dbase_config FOREIGN KEY (dbase_config) REFERENCES resources
+	ON DELETE NO ACTION
+	ON UPDATE CASCADE
+);
+
+GRANT SELECT ON groups,resources,group_resources TO apache;
+
+CREATE UNLOGGED TABLE log (
+timestamp timestamp NOT NULL,
+ip_address text NOT NULL,
+method text NOT NULL,
+route text NOT NULL,
+duration float NOT NULL
+);
+
+GRANT SELECT,UPDATE,INSERT,DELETE ON log TO apache,bigsdb;
+CREATE INDEX l_l1 ON log USING brin(timestamp);
+
+
+
+-- migrate:down
+
