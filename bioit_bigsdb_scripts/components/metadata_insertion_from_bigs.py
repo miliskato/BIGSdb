747c1a62f0e609b64ec09adb006bd1a4ed9b39d8
angori <ann-stephan.gori@sciensano.be>
2023-11-21 12:20:07 +0100

    fix multiple submissions

--- bioit_bigsdb_scripts/components/metadata_insertion_from_bigs.py
+++ bioit_bigsdb_scripts/components/metadata_insertion_from_bigs.py
@@ -3,7 +3,7 @@ import logging
 import socket
 import sys
 from pathlib import Path
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Any
 
 PYTHONPATH = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(PYTHONPATH))
@@ -24,19 +24,31 @@ def parse_arguments(specieslist: List[str]) -> argparse.Namespace:
     return argument_parser.parse_args()
 
 
-def get_lab_metadata_dictionary(lab_metadata_from_query: List[Tuple]) -> Tuple[Dict[str, str], str]:
+def convert_submission_to_dict(submission_result: List[Tuple]) -> dict:
     """
     Handle results of the psql request which get back submissions from BIGSdb uploading tool
-    :param lab_metadata_from_query: list of tuples containing lab metadata from the "isolate_submission_isolates" table.
+    :param submission_result: list of tuples containing lab metadata from the "isolate_submission_isolates" table.
+    :return: Dictionary containing lab_metadata and string returning the isolate id.
+    """
+    submissions_by_index = {}
+    for i, item in enumerate(submission_result):
+        submissions_by_index.setdefault(item[1], []).append(item[2:])
+    return submissions_by_index
+
+
+def get_lab_metadata_dictionary(lab_metadata_by_isolate: List[Tuple]) -> Tuple[Dict[str, str], str]:
+    """
+    Handle results of the psql request which get back submissions from BIGSdb uploading tool
+    :param lab_metadata_by_isolate: list of tuples containing metada info for a single isolate.
     :return: Dictionary containing lab_metadata and string returning the isolate id.
     """
     metadata_dictionary = {}
     isolate_str = ''
-    for i, item in enumerate(lab_metadata_from_query):
-        if lab_metadata_from_query[i][2] == 'isolate':
-            isolate_str = lab_metadata_from_query[i][3]
+    for i, item in enumerate(lab_metadata_by_isolate):
+        if lab_metadata_by_isolate[i][0] == 'isolate':
+            isolate_str = lab_metadata_by_isolate[i][1]
         else:
-            metadata_dictionary[lab_metadata_from_query[i][2]] = lab_metadata_from_query[i][3]
+            metadata_dictionary[lab_metadata_by_isolate[i][0]] = lab_metadata_by_isolate[i][1]
     if isolate_str == '':
         send_email(f"Insertion failed: Isolate_id was missing for this submission, BigsDB should have refused the submission!",
                    subject=f"Metadata insertion failure on host {socket.gethostname()}")
@@ -60,17 +72,20 @@ def insert_lab_metadata_through_bigs(species: str) -> None:
         for submitted_id in submission_ids:
             lab_metadata = isolates_isosubiso_psql_tbl.get_field_and_value_from_submission(submitted_id)
 
-            metadata_dict, isolate_id = get_lab_metadata_dictionary(lab_metadata)
+            dict_by_isolate = convert_submission_to_dict(lab_metadata)
 
-            if isolate_id in isolates_already_in_bigs:
-                # prepare query
-                isolate_update_query = TblIsolates.build_update_nomin_metadata_query(metadata_dict)
-                value_to_set_in_fields = [v for v in metadata_dict.values()]
-                value_to_set_in_fields.append(isolate_id)
-                isolates_psql_tbl.update_nomin_metadata(isolate_update_query, value_to_set_in_fields)
-            else:
-                send_email(f"Insertion failed: Isolate {isolate_id} not found in bigsDB",
-                           subject=f"Metadata insertion failure on host {socket.gethostname()}")
+            for key in dict_by_isolate:
+                metadata_dict, isolate_id = get_lab_metadata_dictionary(dict_by_isolate[key])
+
+                if isolate_id in isolates_already_in_bigs:
+                    # prepare query
+                    isolate_update_query = TblIsolates.build_update_nomin_metadata_query(metadata_dict)
+                    value_to_set_in_fields = [v for v in metadata_dict.values()]
+                    value_to_set_in_fields.append(isolate_id)
+                    isolates_psql_tbl.update_nomin_metadata(isolate_update_query, value_to_set_in_fields)
+                else:
+                    send_email(f"Insertion failed: Isolate {isolate_id} not found in bigsDB",
+                               subject=f"Metadata insertion failure on host {socket.gethostname()}")
 
             isolates_sub_psql_tbl.update_submission(submitted_id)
 
