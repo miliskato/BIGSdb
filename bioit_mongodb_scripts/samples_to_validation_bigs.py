55c127027744aad27865608de6cebb8f9d8929be
angori <ann-stephan.gori@sciensano.be>
2023-11-30 11:13:32 +0100

    Remove hardlink to report - replaced by report_directory

--- bioit_mongodb_scripts/samples_to_validation_bigs.py
+++ bioit_mongodb_scripts/samples_to_validation_bigs.py
@@ -27,11 +27,7 @@ def _insert_submission_bigs(sample_docs: List[Dict[str, Any]], validation_type: 
             TblIsolateSubmissionFieldOrder(species) as isolates_isosubfo_psql_tbl:
         for doc in sample_docs:
             isolates_sub_psql_tbl.insert_submission((validation_type,))
-            # todo need to set a proper method to build links based on the sample to transfer
-            # for testing purposes
-            html_path = 'http://bioit-bigs-dev.sciensano.be/galaxyreports/listeria/110-001_S68_L001/report.html'
-            # dev code, not set yet
-            html_path = str(html_path).replace('/reports/', '/galaxyreports/') #  todo, probably needs to be removed after proper working with htmlreporter
+            html_path = doc['report_directory']
             html_link = f'<p><a href="{html_path}" target="_blank"> html report</a></p>'
             # end of dev code
             isolates_isosubiso_psql_tbl.insert_validation_metadata(('html_report', html_link))
