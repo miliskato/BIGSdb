d00235d37d4dc88bed3c37df549aa865da47bd86
AnGo5687 <ann-stephan.gori@sciensano.be>
2023-12-18 11:43:10 +0100

    fix migration for postgis

--- sql/bigsdb_isolates_v/db/migrations/20231204095710_create_geography_table.sql
+++ sql/bigsdb_isolates_v/db/migrations/20231204095710_create_geography_table.sql
@@ -1,4 +1,6 @@
 -- migrate:up
+CREATE EXTENSION postgis;
+
 CREATE TABLE geography_point_lookup (
 id bigserial NOT NULL,
 country_code text NOT NULL,
@@ -19,3 +21,4 @@ GRANT SELECT,UPDATE,INSERT,DELETE ON geography_point_lookup TO apache;
 
 -- migrate:down
 DROP TABLE geography_point_lookup;
+DROP EXTENSION postgis;
