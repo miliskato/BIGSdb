f1bf226657b3158fc6b960e2b12543a302b52287
mikelchtermans <michael.kelchtermans@sciensano.be>
2024-01-23 12:31:49 +0100

    fix bug with Typing in docstring of databaseconnection.py that I introduced not long ago

--- bioit_bigsdb_scripts/components/psql/databaseconnection.py
+++ bioit_bigsdb_scripts/components/psql/databaseconnection.py
@@ -39,7 +39,7 @@ class DatabaseConnection:
         self._cursor: psycopg2.extensions.cursor = self._connection.cursor()
         self.name = self._cursor.name
 
-    def execute_query(self, query: str, params: Union[Tuple[Union[str, int], ...], List[Union[str, int], ...]]) \
+    def execute_query(self, query: str, params: Union[Tuple[Union[str, int, Tuple[str]]], List[Union[str, int]]]) \
             -> Optional[List[Optional[Tuple[Any]]]]:
         """
         Executes a sql query using psycopg2 sanitazation
