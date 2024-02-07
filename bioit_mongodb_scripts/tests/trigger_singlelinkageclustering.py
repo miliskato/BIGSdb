6dc05e21cc03de8ada7c5d7763ac1e601d1a32de
mikelchtermans <michael.kelchtermans@sciensano.be>
2024-02-07 14:44:31 +0100

    Add script that will trigger the single linkage clustering from scratch and insert into mongodb

---
+++ bioit_mongodb_scripts/tests/trigger_singlelinkageclustering.py
@@ -0,0 +1,25 @@
+# Usage : python trigger_singlelinkageclustering.py 5 10
+import sys
+from pathlib import Path
+
+PYTHONPATH = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(PYTHONPATH))
+
+from bioit_mongodb_scripts.util.python_utility_functions import get_mongodb_config_data
+
+if __name__ == '__main__':
+
+    # Parse config
+    mongo_config_data = get_mongodb_config_data()
+
+    from bioit_mongodb_scripts.util.distance_and_cluster_computer import DistanceAndClusterComputer
+    distance_cluster = DistanceAndClusterComputer('mycobacterium', mongo_config_data)
+
+    threshold_list = []
+    n = len(sys.argv)
+
+    for i in range(1, n):
+        threshold_list.append(int(sys.argv[i]))
+
+    distance_cluster.compute_hamming_distances('full')
+    distance_cluster.init_clustering_and_cluster_membership(set(threshold_list))
