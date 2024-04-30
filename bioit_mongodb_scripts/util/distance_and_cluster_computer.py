ef534575bc5eb43203e430ded1d02c1f17166d23
mikelchtermans <michael.kelchtermans@sciensano.be>
2024-04-30 14:08:26 +0200

    actual bugfix clustering

--- bioit_mongodb_scripts/util/distance_and_cluster_computer.py
+++ bioit_mongodb_scripts/util/distance_and_cluster_computer.py
@@ -211,14 +211,10 @@ class DistanceAndClusterComputer:
         """
         for thresh in cluster_thresholds:
             membership = []
-            for index, hamming_distance in enumerate(self._hamming_distances[0][:-1]):
+            for index, hamming_distance in enumerate(self._hamming_distances[0][:-2]):
                 if hamming_distance <= thresh:
-                    membership_doc = self._cluster_membership_collection.find_one({'cgST': self._sequence_types[index],
-                                                                                   'threshold': thresh})
-                    if membership_doc is not None and membership_doc.get('clustering_membership'):
-                        membership.append(membership_doc['clustering_membership'])
-                    else:
-                        logging.warning(f"Queried cgST {self._sequence_types[index]}'s clustering membership for threshold {thresh} but could not find any...")
+                    membership.append(self._cluster_membership_collection.find_one({'cgST': self._sequence_types[index],
+                                                                                    'threshold': thresh})['clustering_membership'])
             membership = list(set(membership))
             if len(membership) > 1:
                 membership = [self._merge_clusters(membership, thresh)]
