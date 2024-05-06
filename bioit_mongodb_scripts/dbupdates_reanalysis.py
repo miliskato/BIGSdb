872e60bfbe1fc54a695e51e8ac781f4ea263a02e
kamilis <kato.milis@sciensano.be>
2024-05-06 14:04:02 +0200

    Modify dbupdates-reanalysis script to connect to two environments

--- bioit_mongodb_scripts/dbupdates_reanalysis.py
+++ bioit_mongodb_scripts/dbupdates_reanalysis.py
@@ -5,6 +5,7 @@ import time
 from datetime import datetime, timedelta
 from pathlib import Path
 
+from azure import batch
 from azure.monitor.ingestion import LogsIngestionClient
 
 from bioit_mongodb_scripts.reanalysis.reanalysis_submitter_azure import BatchPipelinesReanalysis
@@ -20,38 +21,55 @@ DEALLOCATE_VM = 'az login --identity; az vm deallocate -n $(hostname) -g $(curl 
 
 class DbUpdatesReanalysis:
     """
-    This class disables jobs in the batch account of the given environment, checks whether database updates can be
+    This class disables jobs in the batch accounts of the given environments, checks whether database updates can be
     carried out, executes the database updates, enables jobs again, launches the reanalysis and eventually
     deallocates the dbupdates VM.
     """
 
-    def __init__(self, dtap: str) -> None:
+    def __init__(self, environment: str) -> None:
         """
-        Initialises the class, connects to the keyvault and batch account associated with the environment
+        Initialises the class, connects to the keyvaults and batch accounts associated with the environment
         and executes the main functions.
-        :param dtap: dev, test, acc, or prod
+        :param environment: dt or ap
         :return: None
         """
         try:
             filename_log = f"/var/log/dbupdate-logs/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_log.txt"
             logging.basicConfig(filename=filename_log, filemode='w', level=logging.DEBUG,
                                 format='%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s')
-            self._dtap = dtap
+            self._environment = environment
             self._mongo_config_data = get_mongodb_config_data()
-            self._connection_azure = ConnectAzure(self._dtap)
-            self._batch_client = self._connection_azure.connect_to_batch_client()
-            self._disable_batch_jobs()
+            self._connection_azure_1, self._connection_azure_2 = self._connect_azure()
+            self._batch_client_1 = self._connection_azure_1.connect_to_batch_client()
+            self._batch_client_2 = self._connection_azure_2.connect_to_batch_client()
+            self._disable_batch_jobs(self._batch_client_1)
+            self._disable_batch_jobs(self._batch_client_2)
             self._execute_dbupdates()
-            self._enable_batch_jobs()
+            self._enable_batch_jobs(self._batch_client_1)
+            self._enable_batch_jobs(self._batch_client_2)
             self._execute_reanalysis()
         except Exception as e:
-            self._enable_batch_jobs()
+            self._enable_batch_jobs(self._batch_client_1)
+            self._enable_batch_jobs(self._batch_client_2)
             logging.error(f"An exception occurred during the database updates and the automated reanalysis: {e}")
             message_log = "An error occurred during the database updates and launching of the reanalysis"
             self._send_log_entry(level="Error", exception_message=str(e), message=message_log,
                                  stream_name="Custom-DbupdatesReanalysis_CL")
         finally:
             self._execute_command(DEALLOCATE_VM)
+
+    def _connect_azure(self) -> tuple[ConnectAzure, ConnectAzure]:
+        """"
+        Connects to the keyvaults of the environments (dt or ap).
+        return: connections to the keyvaults
+        """
+        if self._environment == 'dt':
+            connection_azure_1 = ConnectAzure("dev")
+            connection_azure_2 = ConnectAzure("test")
+        else:
+            connection_azure_1 = ConnectAzure("acc")
+            connection_azure_2 = ConnectAzure("prod")
+        return connection_azure_1, connection_azure_2
 
     def _execute_dbupdates(self) -> None:
         """
@@ -74,8 +92,10 @@ class DbUpdatesReanalysis:
         """
         start_time = datetime.now()
         while datetime.now() - start_time < timedelta(hours=max_hours):
-            self.__get_task_status()
-            if any(status == "running" for status in self._tasks_status):
+            tasks_status_1 = self.__get_task_status(self._batch_client_1)
+            tasks_status_2 = self.__get_task_status(self._batch_client_2)
+            combined_status = tasks_status_1 + tasks_status_2
+            if any(status == "running" for status in combined_status):
                 logging.info("Still tasks running, database updates can't be carried out")
                 time.sleep(sleep_minutes * 60)
             else:
@@ -92,7 +112,12 @@ class DbUpdatesReanalysis:
         schemes = ['mlst', 'cgmlst', 'mlst_warwick', 'mlst_pasteur']
         for species in self._mongo_config_data['species']:
             for scheme in schemes:
-                TempidReplacerAzure(scheme, species, self._dtap)
+                if self._environment == 'dt':
+                    TempidReplacerAzure(scheme, species, "dev")
+                    TempidReplacerAzure(scheme, species, "test")
+                else:
+                    TempidReplacerAzure(scheme, species, "acc")
+                    TempidReplacerAzure(scheme, species, "prod")
 
     def _execute_reanalysis(self) -> None:
         """
@@ -100,23 +125,30 @@ class DbUpdatesReanalysis:
         :return: None
         """
         for species in self._mongo_config_data['species']:
-            BatchPipelinesReanalysis(species, self._dtap)
+            if self._environment == 'dt':
+                BatchPipelinesReanalysis(species, "dev")
+                BatchPipelinesReanalysis(species, "test")
+            else:
+                BatchPipelinesReanalysis(species, "acc")
+                BatchPipelinesReanalysis(species, "prod")
 
-    def _disable_batch_jobs(self, retries: int = 3, timeout: int = 60 * 5, disable_tasks: str = "wait") -> None:
+    @staticmethod
+    def _disable_batch_jobs(batch_client: batch.BatchServiceClient, retries: int = 3, timeout: int = 60 * 5, disable_tasks: str = "wait") -> None:
         """
         Disables all the jobs of a specific batch account. Default of disable tasks is wait which means that currently
         running tasks are allowed to complete, while newly added tasks are queued.
+        :param batch_client: Batch account
         :param retries: Number of retries
         :param timeout: Waiting time until trying again
         :param disable_tasks: requeue, terminate or wait
         :return: None
         """
-        for job in self._batch_client.job.list():
+        for job in batch_client.job.list():
             job_disabled = False
             for retry in range(retries):
                 try:
                     if job.state not in ('disabled', 'disabling'):
-                        self._batch_client.job.disable(job.id, disable_tasks)
+                        batch_client.job.disable(job.id, disable_tasks)
                         logging.info(f"Job {job.id} has been successfully disabled.")
                         job_disabled = True
                         break
@@ -131,19 +163,20 @@ class DbUpdatesReanalysis:
             if not job_disabled:
                 raise Exception(f"Job {job.id} could not be disabled")
 
-    def _enable_batch_jobs(self, retries: int = 3, timeout: int = 60 * 5) -> None:
+    def _enable_batch_jobs(self, batch_client: batch.BatchServiceClient, retries: int = 3, timeout: int = 60 * 5) -> None:
         """
         Enables all the jobs of a specific batch account.
+        :param batch_client: Batch account
         :param retries: Number of retries
         :param timeout: Waiting time until trying again
         :return: None
         """
-        for job in self._batch_client.job.list():
+        for job in batch_client.job.list():
             job_enabled = False
             for retry in range(retries):
                 try:
                     if job.state not in ('active', 'enabling'):
-                        self._batch_client.job.enable(job.id)
+                        batch_client.job.enable(job.id)
                         logging.info(f"Job {job.id} has been successfully enabled.")
                         job_enabled = True
                         break
@@ -160,19 +193,21 @@ class DbUpdatesReanalysis:
                 self._send_log_entry(level="Error", exception_message=message_log, message=message_log,
                                      stream_name="Custom-DbupdatesReanalysis_CL")
 
-    def __get_task_status(self) -> None:
+    @staticmethod
+    def __get_task_status(batch_client: batch.BatchServiceClient) -> list:
         """
         Gets the status of all the tasks for all the jobs of a specific batch account.
-        :return: None
+        :param batch_client: batch account
+        :return: list with the statuses of all tasks of a specific batch account
         """
         tasks_status = []
-        for job in self._batch_client.job.list():
+        for job in batch_client.job.list():
             # Get the list of tasks for the current job
-            tasks = self._batch_client.task.list(job.id)
+            tasks = batch_client.task.list(job.id)
             for task in tasks:
                 task_status = task.state
                 tasks_status.append(task_status)
-        self._tasks_status = tasks_status
+        return tasks_status
 
     def _send_log_entry(self, level: str, exception_message: str, message: str, stream_name: str):
         """
@@ -182,15 +217,15 @@ class DbUpdatesReanalysis:
         param message: information message
         param stream_name: Table to which the log is written
         """
-        endpoint = self._connection_azure.get_secret_value('DATA-COLLECTION-ENDPOINT-DBUPDATES')
-        client = LogsIngestionClient(endpoint=endpoint, credential=self._connection_azure.credential)
-        rule_id = self._connection_azure.get_secret_value('DBUPDATES-REANALYSIS-RULE-ID')
+        endpoint = self._connection_azure_1.get_secret_value('DATA-COLLECTION-ENDPOINT-DBUPDATES')
+        client = LogsIngestionClient(endpoint=endpoint, credential=self._connection_azure_1.credential)
+        rule_id = self._connection_azure_1.get_secret_value('DBUPDATES-REANALYSIS-RULE-ID')
 
         time_generated = datetime.now().isoformat()
         body = [{
             "TimeGenerated": time_generated,
             "Level": level,
-            "Environment": self._dtap,
+            "Environment": self._environment,
             "ExceptionMessage": exception_message,
             "Message": message
         }]
@@ -215,6 +250,6 @@ class DbUpdatesReanalysis:
 if __name__ == "__main__":
     hostname = socket.gethostname()
     if 'dt' in hostname:
-        DbUpdatesReanalysis(dtap='dev')
+        DbUpdatesReanalysis(environment='dt')
     else:
         print("Execution of dbupdates and launching of reanalysis not yet implemented on ap.")
