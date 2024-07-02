f6d518407ae069e8563e6318ef6f97b6188a779f
angori <ann-stephan.gori@sciensano.be>
2024-07-02 13:52:50 +0200

    add missing param for submission id

--- bioit_bigsdb_scripts/components/psql/psql_queries.py
+++ bioit_bigsdb_scripts/components/psql/psql_queries.py
@@ -354,7 +354,7 @@ class PsqlQueries():
         FROM submissions 
         LEFT JOIN users ON users.id = submissions.curator 
         LEFT JOIN isolate_submission_isolates ON isolate_submission_isolates.submission_id = submissions.id 
-        WHERE submissions.status='closed' and isolate_submission_isolates.field='isolate_id';"""
+        WHERE submissions.status='closed' and isolate_submission_isolates.field='isolate_id' and submissions.id=%s;"""
     ISO_UPD_STATUS_TB_SUB_VAR_ID: Final[str] = """
         UPDATE submissions SET status='validation_sent_to_bioit_platform' WHERE id=%s;"""
     ISO_INS__TB_SUB_VAR_VALTYPE: Final[str] = """
