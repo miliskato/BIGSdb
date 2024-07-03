8889046bc692f3fbba47c08a01fe903586fb1cbf
angori <ann-stephan.gori@sciensano.be>
2024-07-03 16:08:49 +0200

    addressing comments for BIGSDB-CR-24

--- bioit_bigsdb_scripts/components/psql/psql_tbl_jobs.py
+++ bioit_bigsdb_scripts/components/psql/psql_tbl_jobs.py
@@ -15,14 +15,14 @@ class TblJobs(DatabaseConnection):
         """
         super().__init__(None, 'jobs')
 
-    def select_pid_of_started_jobs(self) -> List[Optional[Tuple[int]]]:
+    def select_pid_of_started_jobs(self) -> List[Optional[Tuple[int, str, str]]]:
         """
         Select and return pid for jobs with status "started"
         :return: list of tuples of ints if "started" jobs are found
         """
         return self.execute(PsqlQueries.JOB_SEL_PID_STARTED_JOBS_TB_JOBS)
 
-    def assigned_failed_status(self, param: Tuple[int]) -> None:
+    def assign_failed_status(self, param: Tuple[int]) -> None:
         """
         Set job status with these PIDs to "cancelled"
         :param param: variables to feed to the PSQL query, in this case, pid of jobs which are not running anymore
