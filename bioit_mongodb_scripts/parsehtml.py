b47686f346686f5a1d48a1999effd71c4766b852
kamilis <kato.milis@sciensano.be>
2024-08-13 14:31:25 +0200

    Parse html: arguments to be searched + documentation

--- bioit_mongodb_scripts/parsehtml.py
+++ bioit_mongodb_scripts/parsehtml.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
 import argparse
+import sys
 from pathlib import Path
-from typing import List
+from typing import List, Union
 
 import yaml
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 from bioit_mongodb_scripts.util.python_utility_functions import get_mongodb_config_data
+
+PYTHONPATH = Path(__file__).resolve().parent.parent
+sys.path.append(str(PYTHONPATH))
 
 
 def parse_arguments(specieslist: List[str]) -> argparse.Namespace:
@@ -23,8 +28,19 @@ def parse_arguments(specieslist: List[str]) -> argparse.Namespace:
 
 
 class ParseHtml:
-
-    def __init__(self, html1: Path, html2: Path, analysis_arguments: list, species: str, new_file: Path):
+    """
+    Parses the old and new html report and replaces the sections that changed with regard to the old report.
+    """
+    def __init__(self, html1: Path, html2: Path, analysis_arguments: List[str], species: str, new_file: Path) -> None:
+        """
+        Initializes this class and executes the main function.
+        :param html1: Path to the first html report
+        :param html2: Path to the second html report
+        :param analysis_arguments: analysis arguments
+        :param species: commonly used bioit species name: either genus or specific like stec
+        :param new_file: Path to the new html report
+        :return: None
+        """
         self._html1 = html1
         self._html2 = html2
         self._analysis_arguments = analysis_arguments
@@ -32,40 +48,11 @@ class ParseHtml:
         self._new_file = new_file
         self._adapt_html()
 
-    def __change_arguments(self):
-        new_arguments = []
-        for analysis_argument in self._analysis_arguments:
-            with Path('arguments.yml').open('r') as handle:
-                codes_dict = yaml.safe_load(handle)
-                if codes_dict[self._species].get(analysis_argument):
-                    new_arguments.append(codes_dict[self._species][analysis_argument])
-        new_arguments.append('input')
-        return new_arguments
-
-    @staticmethod
-    def __load_html(filename):
-        with open(filename, 'r', encoding='utf-8') as file:
-            soup = BeautifulSoup(file, 'lxml')
-        return soup
-
-    # Function to find a section by header text, traversing up the DOM tree to find the container div
-    @staticmethod
-    def ___find_section_by_header(soup, header_text):
-        target_sections = soup.find_all('div', {"class": "report_section"})
-        for section in target_sections:
-            # print(section)
-            if (section.h2 and header_text == section.h2.text) or (section.h3 and header_text == section.h3.text):
-                return section
-
-    def __replace_section(self, soup1, soup2, header_text):
-        section_to_replace = self.___find_section_by_header(soup1, header_text)
-        new_section = self.___find_section_by_header(soup2, header_text)
-        # Replace these section
-        if section_to_replace and new_section:
-            section_to_replace.replace_with(new_section)
-        return soup1
-
-    def _adapt_html(self):
+    def _adapt_html(self) -> None:
+        """
+        Adapts the html report.
+        :return: None
+        """
         soup1 = self.__load_html(self._html1)
         soup2 = self.__load_html(self._html2)
         new_arguments = self.__change_arguments()
@@ -73,6 +60,65 @@ class ParseHtml:
             soup1 = self.__replace_section(soup1, soup2, analysis_argument)
         with open(self._new_file, 'w', encoding='utf-8') as file:
             file.write(str(soup1))
+
+    @staticmethod
+    def __load_html(filename: Path) -> BeautifulSoup:
+        """
+        Loads the html report into a BeautifulSoup object.
+        :param filename: Path to the file that has to be loaded
+        :return: BeautifulSoup object
+        """
+        with open(filename, 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file, 'lxml')
+        return soup
+
+    def __change_arguments(self) -> List[str]:
+        """
+        Changes the arguments into the strings used in the header of the report.
+        :return: list of the changed arguments
+        """
+        new_arguments = []
+        with Path('arguments.yml').open('r') as handle:
+            codes_dict = yaml.safe_load(handle)
+            if self._species in codes_dict:
+                for analysis_argument in self._analysis_arguments:
+                    if analysis_argument in codes_dict[self._species]:
+                        value = codes_dict[self._species][analysis_argument]
+                        if isinstance(value, list):
+                            new_arguments.extend(value)
+                        else:
+                            new_arguments.append(value)
+        new_arguments.append('Input')
+        return new_arguments
+
+    def __replace_section(self, soup1: BeautifulSoup, soup2: BeautifulSoup, header_text: str) -> BeautifulSoup:
+        """
+        Replaces a section of the first BeautifulSoup object by a section of the second BeautifulSoup object.
+        :param soup1: first BeautifulSoup object
+        :param soup2: second BeautifulSoup object
+        :return: the first BeautifulSoup object which contains the replaced section
+        """
+        section_to_replace = self.___find_section_by_header(soup1, header_text)
+        new_section = self.___find_section_by_header(soup2, header_text)
+        # Replace these section
+        if section_to_replace and new_section:
+            section_to_replace.replace_with(new_section)
+        return soup1
+
+    @staticmethod
+    def ___find_section_by_header(soup: BeautifulSoup, header_text: str) -> Union[Tag, None]:
+        """
+        Finds a section of the report by using the header text.
+        :param soup: BeautifulSoup object that has to be searched
+        :param header_text: header text that has to be found
+        :return: either a Tag object (if a matching section is found) or None (if no matching section is found)
+        """
+        target_sections = soup.find_all('div', {"class": "report_section"})
+        for section in target_sections:
+            if (section.h2 and header_text == section.h2.text) or (section.h3 and header_text == section.h3.text):
+                return section
+            if section.p and section.p.text.startswith(header_text):
+                return section
 
 
 if __name__ == '__main__':
@@ -82,9 +128,9 @@ if __name__ == '__main__':
     # Parse arguments
     args = parse_arguments(mongo_config_data['species'])
 
+    # Run main
     ParseHtml(args.html1,
               args.html2,
               args.analysis_arguments,
               args.species,
               args.new_file)
-
