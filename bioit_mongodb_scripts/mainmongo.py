b63f24181407dca57b87f0f885ba03222b847ce5
kamilis <kato.milis@sciensano.be>
2024-09-03 09:24:21 +0200

    Adapted is_viral to config

--- bioit_mongodb_scripts/mainmongo.py
+++ bioit_mongodb_scripts/mainmongo.py
@@ -93,7 +93,6 @@ class MainMongo:
         self._technical_id = technical_id
         self._species = species
         self._pipeline_hash = pipeline_hash
-        self._is_viral = self._species in ['influenza_a', 'influenza_b', 'sars_cov_2']
         self._results_type = results_type
         self._technical_metadata_path = technical_metadata_path
         self._jsonfilepath = jsonfilepath
@@ -228,7 +227,7 @@ class MainMongo:
         """
         new_records["isolates_id"] = self._technical_id
         good_sample_quality = True
-        if self._results_type == 'new_isolate' and not self._is_viral:  # viral pathogens do not have a qc section
+        if self._results_type == 'new_isolate' and self._species not in self._mongo_config_data['viral_species']:  # viral pathogens do not have a qc section
             try:
                 for qc_type in new_records['qc']:
                     for key in new_records['qc'][qc_type]:
