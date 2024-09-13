2bd5132344b02a235f2721a90168dcbdb1271746
angori <ann-stephan.gori@sciensano.be>
2024-09-13 17:31:51 +0200

    remove unused dictionaries

--- bioit_mongodb_scripts/util/alerts_to_bigs.py
+++ bioit_mongodb_scripts/util/alerts_to_bigs.py
@@ -248,7 +248,6 @@ class AlertsToBigs:
                 if investigation_method == 'distance matrix':
                     queried_isolates = self._isolates_psql_tbl.select_isolates_by_cgsts(
                         (self._cgmlst_bigsdb_scheme_id, cgsts_as_tuple_of_str))
-                    self._cgst_isolatecount_dict[cgst] = len(queried_isolates)
 
                 else:  # investigation_method == 'single linkage':
                     queried_isolates = self._isolates_psql_tbl.select_isolates_by_cluster_group(
@@ -258,16 +257,14 @@ class AlertsToBigs:
                     # all cluster groups are the same in the query; take 4th element (cluster group) of first
                     # tuple, which always has to exist because the isolate itself is definitely queried)
                     # Condition to skip it for first insertion of new cgST clust in which case the tuple is empty
-                    if len(queried_isolates) >= 1:
-                        self._clgr_isolatecount_dict[queried_isolates[0][4]] = len(queried_isolates)
+
             else:
                 start_date = (isolation_date - self._timedelta_timeframe).strftime('%Y-%m-%d')
                 end_date = (isolation_date + self._timedelta_timeframe).strftime('%Y-%m-%d')
                 if investigation_method == 'distance matrix':
                     queried_isolates = self._isolates_psql_tbl.select_isolates_by_cgsts_and_between_dates(
                         (self._cgmlst_bigsdb_scheme_id, cgsts_as_tuple_of_str, start_date, end_date))
-                    self._cgst_date_isolatecount_dict[(cgst, str(isolation_date))] = \
-                        len(queried_isolates)
+
 
                 else:  # investigation_method == 'single linkage':
                     queried_isolates: list[Optional[tuple[Any]]] = self._isolates_psql_tbl. \
@@ -275,8 +272,7 @@ class AlertsToBigs:
                         (self._bigsdb_config_data['alerts'][self._species][
                              f'{threshold_key}_classification_scheme_id'],
                          self._cgmlst_bigsdb_scheme_id, str(cgst), start_date, end_date))
-                    self._clgr_date_isolatecount_dict[(queried_isolates[0][4], str(isolation_date))] = \
-                        len(queried_isolates)
+
             return queried_isolates
 
     def ___update_identifiers_for_alert(self, alert_id: str, new_isolate_bigsdb_id: str, isolate_name: str) -> None:
