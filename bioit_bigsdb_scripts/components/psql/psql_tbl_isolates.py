8b5a4599bde381acf4d78f1fb72a7efa1e44d659
mikelchtermans <michael.kelchtermans@sciensano.be>
2024-10-10 14:14:01 +0200

    Modify alerts_to_bigs for new versions part 2: fix mistake

--- bioit_bigsdb_scripts/components/psql/psql_tbl_isolates.py
+++ bioit_bigsdb_scripts/components/psql/psql_tbl_isolates.py
@@ -71,11 +71,11 @@ class TblIsolates(DatabaseConnection):
         """
         self.execute_query(PsqlQueries.ISO_UPD_NEWV_TB_ISO_VAR_ISO, param)
 
-    def select_current_cgst_of_isolate(self, param: Tuple[int, str]) -> Optional[Tuple[Any]]:
+    def select_current_cgst_of_isolate(self, param: Tuple[str]) -> List[Optional[Tuple[Any]]]:
         """
         Select the cgst found in BIGSdb for the current isolate and return it along with its isolate id
         :param param: cgmlst scheme id, cgmlst scheme id, isolate name
-        :return: None or tuple containing the isolate_id and its cgst
+        :return: None or list of tuple containing the cgst
         """
         return self.execute_query(PsqlQueries.ISO_SEL_CGST_TB_ISO_VAR_SCHID_ISO, param)
 
