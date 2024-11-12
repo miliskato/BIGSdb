69349d234bec86aa518a4a0d75bdbd4a9b81058d
angori <ann-stephan.gori@sciensano.be>
2024-11-12 14:48:28 +0100

    bugfix reanalysis for badqc_validated

--- bioit_mongodb_scripts/mongo_to_bigs.py
+++ bioit_mongodb_scripts/mongo_to_bigs.py
@@ -333,7 +333,7 @@ class MongoToBigs:
             results_type = "new_isolate"
         elif document.get_validation_type():
             results_type = document.get_validation_type()
-            if results_type == 'resequencing':
+            if results_type == 'resequencing' or (results_type == 'badqc' and sample_presence[0][0] == 1):
                 different_version, cgst_changed = self.___check_if_reanalysis_different(document, isolate_id)
         else:
             results_type = "reanalysis"
