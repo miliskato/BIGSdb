53d9836e1335615f660a99a5ee34cabf874768aa
angori <ann-stephan.gori@sciensano.be>
2024-11-19 12:23:52 +0100

    clean up for MR-68

--- bioit_mongodb_scripts/manually/batch_validation_of_submissions.py
+++ bioit_mongodb_scripts/manually/batch_validation_of_submissions.py
@@ -6,6 +6,9 @@ import traceback
 from pathlib import Path
 from typing import List
 
+PYTHONPATH = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(PYTHONPATH))
+
 from bioit_bigsdb_scripts.components.psql import TblSubmissions
 from bioit_bigsdb_scripts.components.python_utility_functions import get_bigsdb_config_data, send_email
 from bioit_bigsdb_scripts.sample_validation_to_mongo import SampleValidationToMongo
@@ -21,28 +24,30 @@ def parse_arguments(specieslist: List[str]) -> argparse.Namespace:
     argument_parser.add_argument('--accept_all', required=False, type=str, choices=['yes', 'no'])
     return argument_parser.parse_args()
 
+
 class BatchValidationToMongo:
     """
-    This class handles validation/insertion in mongoDB of badqc already pushed in BIGSdb submission table.
+    This class handles validation/insertion in mongoDB of badqcs already pushed in BIGSdb submissions table.
     """
-    def __init__(self, species: str, accept_all: str = 'no') -> None :
+
+    def __init__(self, species: str, accept_all: str = 'no') -> None:
         """
         Initialises the class and runs the main function
         :param species: commonly used bioit species name.
-        :param accept_all: yes/no: if "yes", all badqc still pending for validation in BIGSdb will be accepted.
+        :param accept_all: yes/no: if "yes", all badqcs still pending for validation in BIGSdb will be accepted.
         :return: None
         """
         self.species = species
         self.accept_all = accept_all
 
         try:
-            self.validate_pending_submission_for_badqc()
+            self._validate_pending_submission_for_badqc()
         except Exception as exceptionmessage:
             send_email(f"{exceptionmessage}\n{traceback.format_exc()}",
                        f"{Path(__file__).name} fail on host {socket.gethostname()}")
             raise Exception(f"{exceptionmessage}\n{traceback.format_exc()}")
 
-    def validate_pending_submission_for_badqc(self) -> None:
+    def _validate_pending_submission_for_badqc(self) -> None:
         """
         Method to validate either all the badqc pending for validation in BIGSdb submission table, or only those
         with status and outcome already set to "good" and "closed" by another process.
@@ -50,11 +55,11 @@ class BatchValidationToMongo:
         """
         with TblSubmissions(species=self.species) as isolates_submissions_psql_tbl:
             if self.accept_all == 'yes':
-                isolates_submissions_psql_tbl.validate_pending_badqc()
-            submission_ids=list(isolates_submissions_psql_tbl.get_submission_id_for_validated_badqc())
+                isolates_submissions_psql_tbl.validate_pending_badqcs()
+            submission_ids = list(isolates_submissions_psql_tbl.get_submission_ids_for_validated_badqcs())
 
         for sub_id in submission_ids:
-            SampleValidationToMongo(self.species, sub_id = int(sub_id[0]))
+            SampleValidationToMongo(self.species, sub_id=int(sub_id[0]))
 
 
 if __name__ == '__main__':
@@ -68,4 +73,4 @@ if __name__ == '__main__':
     args = parse_arguments(list(bigsdb_config_data['species_json']))
 
     # run main
-    BatchValidationToMongo(args.species, accept_all = (args.accept_all if args.accept_all else None))
+    BatchValidationToMongo(args.species, accept_all=(args.accept_all if args.accept_all else None))
