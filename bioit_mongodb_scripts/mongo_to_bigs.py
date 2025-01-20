8b6fd11f43d262f266324389113809c0c9ab1aec
Ann-Stephan Gori <ann-stephan.gori@sciensano.be>
2025-01-20 13:53:40 +0000

    clean code - remove unnecessary condition in mongo_to_bigs.py

--- bioit_mongodb_scripts/mongo_to_bigs.py
+++ bioit_mongodb_scripts/mongo_to_bigs.py
@@ -349,7 +349,7 @@ class MongoToBigs:
                 results_type = "new_isolate"
         elif document.get_validation_type():
             results_type = document.get_validation_type()
-            if results_type == 'resequencing' or (results_type == 'badqc' and sample_presence[0][0] == 1):
+            if results_type == 'resequencing' or results_type == 'badqc':
                 different_version, cgst_changed = self.___check_if_reanalysis_different(document, isolate_id)
                 if results_type == 'badqc':
                     results_type = "reanalysis"
