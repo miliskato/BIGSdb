359d8fce7e1371361a391fc52eb5f511f4aa9160
mikelchtermans <michael.kelchtermans@sciensano.be>
2025-03-06 14:56:40 +0100

    improve function name

--- bioit_nrc_integration/python/send_genomic_to_ODS.py
+++ bioit_nrc_integration/python/send_genomic_to_ODS.py
@@ -83,14 +83,14 @@ class SendGenomicToODS(SFTPConnection):
                 if variable_info.get('code_list'):
                     data_dict[variable] = self._translation_codes['code_lists'][variable_info['code_list']][data_dict[variable]]
             if self._species == 'influenza':
-                self.__add_influenza_a_hgna_info(data_dict)
+                self.__add_influenza_a_ha_na_info(data_dict)
         return {'metadata': {'version': self._translation_codes['pathogens'][self._species]['dcd_version'],
                              'data_collection': self._translation_codes['pathogens'][self._species]['dcd_code'],
                              'dcd_name': self._translation_codes['pathogens'][self._species]['dcd_name']},
                 'data': data_dict}
 
     @staticmethod
-    def __add_influenza_a_hgna_info(data_dict: dict[str, Any]) -> None:
+    def __add_influenza_a_ha_na_info(data_dict: dict[str, Any]) -> None:
         """
         Checks if the pathogen is influenza A, then checks if the Type matches HxNx, and if it does, splits HxNx into Hx
         and Nx and adds these to their corresponding variables in the data dictionary to be sent.
