d700898c4df0371db3973c3e47655c93f1f0cb54
angori <ann-stephan.gori@sciensano.be>
2025-03-07 10:30:26 +0100

    cleaning sql insert salmonella

--- bioit_bigsdb_scripts/bigsdb_neisseria_isolates.sql
+++ bioit_bigsdb_scripts/bigsdb_neisseria_isolates.sql
@@ -26,14 +26,14 @@ INSERT INTO scheme_fields(scheme_id, field, type, description, field_order, drop
 
 
 INSERT INTO eav_fields_hidden(field, value_format, description, no_curate, no_submissions, datestamp, curator) VALUES('mongo_results_version', 'text', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
-INSERT INTO eav_fields_hidden(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('ResFinder4', 'text', 'Gene detection', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
+INSERT INTO eav_fields_hidden(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('ResFinder4', 'text', 'ResFinder4', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('html', 'text', 'galaxy report', 'galaxy html report', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('tsv', 'text', 'galaxy report', 'galaxy tsv report', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('assembly', 'text', 'galaxy report', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('VCF_unfiltered', 'text', 'galaxy report', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('VCF_filtered', 'text', 'galaxy report', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
-INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('ResFinder4', 'text', 'Gene detection', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
+INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('ResFinder4', 'text', 'ResFinder 4', '', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('penicillin_SIR_S_frequency', 'text', 'Antibiotic resistance', 'Number of Penicillin susceptible isolates in PubMLST', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('penicillin_SIR_R_frequency', 'text', 'Antibiotic resistance', 'Number of Penicillin resistant isolates in PubMLST', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
 INSERT INTO eav_fields(field, value_format, category, description, no_curate, no_submissions, datestamp, curator) VALUES('rifampicin_SIR_S_frequency', 'text', 'Antibiotic resistance', 'Number of Rifampicin susceptible isolates in PubMLST', 't', 't', (SELECT CURRENT_DATE), 1) ON CONFLICT DO NOTHING;
