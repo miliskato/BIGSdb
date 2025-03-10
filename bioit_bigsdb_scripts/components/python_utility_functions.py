c3e0820b5c94274236a5bf0779ae12b208c0db2e
angori <ann-stephan.gori@sciensano.be>
2025-03-10 15:38:58 +0100

    rm uncompleted dev from this branch

--- bioit_bigsdb_scripts/components/python_utility_functions.py
+++ bioit_bigsdb_scripts/components/python_utility_functions.py
@@ -14,66 +14,6 @@ sys.path.append(str(PYTHONPATH))
 
 from bioit_bigsdb_scripts.config import BIGSDB_CONFIG
 
-class SpeciesTypingSchemeConfig:
-    def __init__(self, data: Dict[str, Union[str, List[Any]]]):
-        self.dirdb = data.get('dirdb')
-        self.scheme_fields = data.get('scheme_fields')
-        self.bigsdb_scheme_name = data.get('bigsdb_scheme_name')
-
-class SpeciesGeneDetectionSchemeConfig:
-    def __init__(self, data: Dict[str, Union[str, List[Any]]]):
-        self.metadata_file = data.get('metadatafile')
-        self.bigsdb_scheme_name = data.get('schemename_bigsdb')
-        self.html_scheme_anchor = data.get('schemename_html')
-
-
-class JsonGeneDetectionSchemeConfig:
-    def __init__(self, data: Dict[str, str]):
-        self.metadata_file = data.get('metadatafile')
-        self.bigsdb_scheme_name = data.get('schemename_bigsdb')
-        self.html_scheme_anchor = data.get('schemename_html')
-
-class SpeciesJsonConfig:
-    def __init__(self, data: Dict[str, Union[str, List[Any], Dict[str, Union[str, Dict[str, Any]]]]]):
-        self.typing_schemes: Dict[str,str] = {}
-        try:
-            for key, value in data.get('typing_schemes', {}).items():
-                self.typing_schemes[key] = value.get('type')
-        except:
-            pass
-
-        self.genedetection_schemes: Dict[str, JsonGeneDetectionSchemeConfig] = {}
-        try:
-            for key, value in data.get('genedetection_schemes').items():
-                self.genedetection_schemes[key] = JsonGeneDetectionSchemeConfig(value)
-        except:
-            pass
-
-class SpeciesConfig:
-    def __init__(self, data: Dict[str, Union[str, List[Any], Dict[str, Union[str, Dict[str, Any]]]]]):
-        self.typing_schemes: Dict[str, SpeciesTypingSchemeConfig] = {}
-        for key, value in data.get['typing_schemes'].items():
-            self.typing_schemes[key] = SpeciesTypingSchemeConfig(value)
-
-        self.genedetection_schemes: Dict[str, SpeciesGeneDetectionSchemeConfig] = {}
-        for key, value in data['genedetection_schemes'].items():
-            self.genedetection_schemes[key] = SpeciesGeneDetectionSchemeConfig(value)
-
-class Config:
-    def __init__(self, data: Dict[str, Union[str, List[Any], Dict[str, Union[str, Dict[str, Any]]]]]):
-        self.data = data
-
-        self.config_species : Dict[str, SpeciesConfig] = {}
-        self.config_species_json : Dict[str, SpeciesJsonConfig] = {}
-        for key, value in data['species_json'].items():
-         self.config_species_json[key] = SpeciesJsonConfig(value)
-
-
-
-
-
-
-
 def get_bigsdb_config_data() -> Dict[str, Union[str, List[Any], Dict[str, Union[str, Dict[str, Any]]]]]:
     """
     Reads the global bigsdb config
@@ -83,9 +23,6 @@ def get_bigsdb_config_data() -> Dict[str, Union[str, List[Any], Dict[str, Union[
         bigsdb_config_data = yaml.safe_load(handle)
     return bigsdb_config_data
 
-def get_bigsdb_config() -> Config:
-    bigsdb_config_data = get_bigsdb_config_data()
-    return Config(bigsdb_config_data)
 
 def send_email(content: str, subject=None,
                config: Dict[str, str] = get_bigsdb_config_data().get('mail'),
@@ -107,7 +44,3 @@ def send_email(content: str, subject=None,
         with smtplib.SMTP(config['host']) as s:
             s.send_message(message)
     logging.debug(content)
-
-if __name__ == '__main__':
-    config = get_bigsdb_config()
-    print(config)
