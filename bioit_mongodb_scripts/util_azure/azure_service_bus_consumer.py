80c72cb188c912761e0d3c1b5c9c6f33c47e52e0
angori <ann-stephan.gori@sciensano.be>
2025-03-18 23:05:47 +0100

    adapt try except azure service bus

--- bioit_mongodb_scripts/util_azure/azure_service_bus_consumer.py
+++ bioit_mongodb_scripts/util_azure/azure_service_bus_consumer.py
@@ -3,7 +3,7 @@ import logging
 import signal
 from typing import Any, List
 
-from azure.servicebus import ServiceBusClient, ServiceBusReceivedMessage, ServiceBusReceiver
+from azure.servicebus import ServiceBusClient, ServiceBusReceivedMessage
 
 from bioit_bigsdb_scripts.components.psql import TblFailedInsertions
 from bioit_mongodb_scripts.mongo_to_bigs import MongoToBigs
@@ -12,9 +12,8 @@ from bioit_mongodb_scripts.util.mongo_initialisation import MongoInitialisation
 from bioit_mongodb_scripts.util.python_utility_functions import get_mongodb_config_data
 from bioit_mongodb_scripts.util.samples_to_validation_bigs import SamplesToValidationBigs
 from bioit_mongodb_scripts.util_azure.azure_service_bus import AzureServiceBus
-
-
 from bioit_mongodb_scripts.util_azure.azure_service_bus_message import AzureServiceBusMessage
+
 
 def parse_arguments(specieslist: List[str]) -> argparse.Namespace:
     """
@@ -27,123 +26,202 @@ def parse_arguments(specieslist: List[str]) -> argparse.Namespace:
     argument_parser.add_argument('--uploader_mail_address', required=True, type=str)
     return argument_parser.parse_args()
 
+
 class Cancellation:
+    """
+    Class to define the cancellation token
+    """
+
     def __init__(self):
+        """
+        initialized the token to false
+        """
         self.cancelled = False
 
-    def cancel(self):
+    def cancel(self) -> None:
+        """
+        Turns the cancellation token to True
+        :return: None
+        """
         self.cancelled = True
 
+class BadCollectionError(Exception):
+    """
+    class to raise error if the collection of the isolates is not handled by the inerter
+    """
+    def __init__(self):
+        super().__init__("The iserter only handles 'isolates' and 'badqc' collections.")
 
 class MessageConsumerDataInserter(AzureServiceBus):
+    """
+    Checks for messages in Azure service bus and inserts pending isolates in BIGSdb
+    """
 
-    def __init__(self, ct: Cancellation, species: str, mongo_config_data: dict[str, Any], uploader_mail_address: str) -> None:
+    def __init__(self, ct: Cancellation, species: str, mongo_config_data: dict[str, Any],
+                 uploader_mail_address: str) -> None:
         """
         Method to initialize the class
         :param ct: Cancellation status: need to exit gracefully if something happens on the VM
+        :param species: Species name
+        :param mongo_config_data: Mongo configuration data
+        :return: None
         """
         super().__init__(mongo_config_data, species)
         self._ct = ct
         self._uploader_mail_address = uploader_mail_address
         self._species = species
 
-
     def execute(self) -> None:
-        with (ServiceBusClient.from_connection_string(conn_str=self._connection_string_asb, logging_enable=True) as service_bus_client):
+        """
+        checks for Azure service bus message and try to insert the isolates notified in these messages
+        :return: None
+        """
+        with (ServiceBusClient.from_connection_string(conn_str=self._connection_string_asb,
+                                                      logging_enable=True) as service_bus_client):
             with service_bus_client.get_queue_receiver(queue_name=self._queue_name) as receiver:
 
                 while not self._ct.cancelled:
                     update_tool = UpdateBIGSdbSeqDef(self._species)
                     update_tool.update_bigsdb_psql_if_needed()
-                    need_to_run_cache = False
+                    should_update_cache = False
                     received_msgs = receiver.receive_messages(max_wait_time=5, max_message_count=1)
 
                     while len(received_msgs) > 0:
+
                         msg = received_msgs[0]
                         if self._ct.cancelled:
                             break
                         pseudo_id = AzureServiceBusMessage.from_json(str(msg)).pseudo_id
-                        collection = AzureServiceBusMessage.from_json(str(msg)).collection
-                        self.__insert_new_message_in_postgres(msg, pseudo_id)
-                        receiver.complete_message(msg)
-                        isolate_id = self._try_return_isolate_identifier(msg, pseudo_id, receiver)
+                        collection_name = AzureServiceBusMessage.from_json(str(msg)).collection
+                        try:
+                            self.__insert_new_message_in_postgres(msg, pseudo_id)
+                            receiver.complete_message(msg)
 
-                        ######✨✨✨✨✨✨🎨🎨🎨🎨🧦🧦🧦 would like to use method but how to change need_to_run_cache
-                        if isolate_id and collection == 'isolates_badqc':
-                            SamplesToValidationBigs(self._species, mongo_config_data=self._mongo_config_data)
+                            isolate_id = self._get_isolate_id(self._species, pseudo_id)
+                            bigs_db_was_modified = self.__insert_known_isolate(collection_name, isolate_id, msg)
 
-                        elif isolate_id and collection == 'isolates':
-                            change_done_in_bigs = self._try_mongo_to_bigs_insertion(isolate_id, msg)
-                            self._rm_msg_from_postgres(msg)
-                            if not need_to_run_cache:
-                                need_to_run_cache = change_done_in_bigs
-                        received_msgs.clear()
-                        received_msgs = receiver.receive_messages(max_wait_time=5, max_message_count=1)
+                            if not should_update_cache:
+                                should_update_cache = bigs_db_was_modified
+                        except IsolateNotFoundException as e:
+                            self.__handle_exception(e,pseudo_id, msg)
+                        except Exception as e:
+                            self.__handle_exception(e,isolate_id, msg)
 
-                    if need_to_run_cache and not received_msgs :
-                        mongo_to_bigs_instance = MongoToBigs(self._species, self._uploader_mail_address)
-                        mongo_to_bigs_instance.cache_and_clustering_update()
+                        received_msgs = receiver.receive_messages(max_wait_time=20, max_message_count=1)
+
+                    if should_update_cache and not received_msgs:
+                        try:
+                            mongo_to_bigs_instance = MongoToBigs(self._species, self._uploader_mail_address)
+                            mongo_to_bigs_instance.cache_and_clustering_update()
+                        except Exception as e:
+                            logging.error('cache update or clustering failed with error: %s', e)
+
+    def __insert_known_isolate(self, collection_name: str, isolate_id: str, msg: ServiceBusReceivedMessage) -> bool :
+        """ inserts isolates for which an isolate_id is known
+        :param collection_name: MongoDB collection to which belongs the isolates
+        :isolate_id: Isolate ID
+        :param msg: a ServiceBusReceivedMessage object
+        """
+        if collection_name == 'isolates_badqc':
+            SamplesToValidationBigs(self._species, mongo_config_data=self._mongo_config_data)
+            return False
+        elif collection_name == 'isolates':
+            return self.__mongo_to_bigs_insertion(isolate_id, msg)
+        else:
+            raise BadCollectionError()
+
+
+    def __handle_exception(self, e: Exception, isolate_id: str, msg: ServiceBusReceivedMessage) -> None:
+        """
+        keep track of the exception in postgres db and also in the /var/log/bigsdb_insertion.log
+        :param e: Exception
+        :param isolate_id: Isolate_id
+        :param msg: a ServiceBusReceivedMessage object
+        """
+        logging.error('%s insertion failed with subsequent error %s', isolate_id, e)
+        template = "An exception of type {0} occurred. Arguments: {1}"
+        exception_msg = template.format(type(e).__name__, e.args)
+        self.__add_exception_to_msg(msg, exception_msg)
 
     def __insert_new_message_in_postgres(self, msg: ServiceBusReceivedMessage, pseudo_id: str) -> None:
         """
-        insert a message in table Failed
+        inserts a message in table failed_insertions from the isolates db.
+        :param msg: a ServiceBusReceivedMessage object
+        :param pseudo_id: the pseudo id of the isolate as found in the msg
+        :return: None
         """
         with TblFailedInsertions(self._species) as psql_tbl_failed_insertions:
             psql_tbl_failed_insertions.insert_message_id((msg.message_id, pseudo_id))
 
-    def _add_exception_to_msg(self, msg: ServiceBusReceivedMessage, exception_msg: str) -> None:
+    def __add_exception_to_msg(self, msg: ServiceBusReceivedMessage, exception_msg: str) -> None:
+        """
+        updates details stored in the table failed_insertions for this message to keep track of the exception
+        :param msg: a ServiceBusReceivedMessage object
+        :param exception_msg: the exception message
+        :return: None
+        """
         with TblFailedInsertions(self._species) as psql_tbl_failed_insertions:
             psql_tbl_failed_insertions.insert_exception_for_message_id((exception_msg, msg.message_id))
 
-    def _rm_msg_from_postgres(self, msg: ServiceBusReceivedMessage) -> None:
+    def __rm_msg_from_postgres(self, msg: ServiceBusReceivedMessage) -> None:
+        """
+        removes entry for this message from the postgres failed_insertion table.
+        :param msg: a ServiceBusReceivedMessage object
+        :return: None
+        """
         with TblFailedInsertions(self._species) as psql_tbl_failed_insertions:
             psql_tbl_failed_insertions.delete_message_id((msg.message_id,))
 
-    def _try_mongo_to_bigs_insertion(self, isolate_id, msg):
-        changes_done_in_bigs = False
-        try:
-            mongo_to_bigs_instance = MongoToBigs(self._species, self._uploader_mail_address,
-                                                 single_sample_id=isolate_id)
-            changes_done_in_bigs = mongo_to_bigs_instance.run_mongo_to_bigs()
-
-        except Exception as e:
-            logging.error('%s insertion failed with subsequent error %s', isolate_id, e)
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            exception_msg = template.format(type(e).__name__, e.args)
-            self._add_exception_to_msg(msg, exception_msg)
-
-        finally:
-            return  changes_done_in_bigs
-
-
-    def _try_return_isolate_identifier(self, msg: ServiceBusReceivedMessage, pseudo_id:str, receiver: ServiceBusReceiver) -> str | None:
+    def __mongo_to_bigs_insertion(self, isolate_id: str, msg: ServiceBusReceivedMessage) -> bool:
         """
-        return the isolate identifier based on the pseudo_id found in message
-        :param msg: one message from the list of ServiceBusReceivedMessage
-        :param receiver: ServiceBusReceiver
+        tries to insert the isolate coming from isolates collection in BIGSdb
+        :param isolate_id: the isolate id of the isolate
+        :param msg: a ServiceBusReceivedMessage object
+        :return: True if the insertion modifies something in BIGSdb else False
         """
-        try:
-            isolate_id = self._get_isolate_identifier(self._species, pseudo_id)
-            return isolate_id
+        mongo_to_bigs_instance = MongoToBigs(self._species, self._uploader_mail_address, single_sample_id=isolate_id)
+        changes_done_in_bigs = mongo_to_bigs_instance.run_mongo_to_bigs()
+        self.__rm_msg_from_postgres(msg)
+        return changes_done_in_bigs
 
-        except Exception as e:
-            logging.error('%s did not get isolate_id from query on mapping collection: %s', pseudo_id, e)
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            exception_msg = template.format(type(e).__name__, e.args)
-            self._add_exception_to_msg(msg, exception_msg)
+    def _get_isolate_id(self, species: str, pseudo_id: str) -> str:
+        """
+        tries to return the isolate_id based on the pseudo_id found in message
+        :param species: the species name
 
-
-
-    def _get_isolate_identifier(self, species: str, pseudo_id: str) -> str:
+        """
         mongo_init_local = MongoInitialisation(species, mongo_config_data=self._mongo_config_data,
                                                selected_connection_string='CONNECTION_STRING_LOCAL')
         mapping_table_collection = mongo_init_local.initialise_mapping_table_collection()
-        isolate_id = mapping_table_collection.find_one({'pseudo_id': pseudo_id})['_id']
+        isolate_id = mapping_table_collection.find_one({'pseudo_id': pseudo_id}).get('_id')
+        if isolate_id is None:
+          raise IsolateNotFoundException(pseudo_id)
         return isolate_id
 
+class IsolateNotFoundException(Exception):
+    """
+    To catch the exception when the pseudo id didn't get its equivalent isolate_id in Mongo
+    """
+    def __init__(self, pseudo_id: str) -> None:
+        """
+        :param pseudo_id: the pseudo id of the isolate
+        :return: None
+        """
+        super().__init__(f'The specified isolate with pseudo_id={pseudo_id} does not exist')
+        self.pseudo_id = pseudo_id
+
 cancel_token = Cancellation()
-def handle_shutdown(signum:int, frame: Any):
+
+
+def handle_shutdown(signum: int, frame: Any) -> None:
+    """
+    Pointer to act on cancel_token when the signal is received.
+    :param signum: int corresponding usually to either SIGINT or SIGTERM
+    :param frame: current stack frame
+    :return: None
+    """
     cancel_token.cancel()
+
 
 if __name__ == '__main__':
 
@@ -152,7 +230,7 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, handle_shutdown)
 
     # Configure stdout logging
-    logging.basicConfig(level=logging.WARNING, filename='/var/log/bigsdb_insertions.log', filemode='w',)
+    logging.basicConfig(level=logging.WARNING, filename='/var/log/bigsdb_insertions.log', filemode='w', )
 
     # Parse Mongo config
     mongo_config_data = get_mongodb_config_data()
@@ -160,7 +238,8 @@ if __name__ == '__main__':
     # Parse arguments
     args = parse_arguments(mongo_config_data['species'])
     try:
-        data_inserter = MessageConsumerDataInserter(cancel_token, args.species, mongo_config_data, args.uploader_mail_address)
+        data_inserter = MessageConsumerDataInserter(cancel_token, args.species, mongo_config_data,
+                                                    args.uploader_mail_address)
         data_inserter.execute()
     except (SystemExit, KeyboardInterrupt):
         pass
