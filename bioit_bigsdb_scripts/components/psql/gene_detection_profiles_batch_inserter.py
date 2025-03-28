457bdad3fa17104c921598b47f0e4d1dc937ccc5
angori <ann-stephan.gori@sciensano.be>
2025-03-28 09:25:18 +0100

    linter

--- bioit_bigsdb_scripts/components/psql/gene_detection_profiles_batch_inserter.py
+++ bioit_bigsdb_scripts/components/psql/gene_detection_profiles_batch_inserter.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass, field
 import socket
+from dataclasses import dataclass, field
 from types import TracebackType
 from typing import List, Tuple, Type, Union
 
@@ -8,29 +8,32 @@ import psycopg2
 from bioit_mongodb_scripts.util.python_utility_functions import send_email
 from .databaseconnection import DatabaseConnection
 
+
 @dataclass
 class GeneDetectionProfilesBatchData:
     """
     This class initializes the lists to store the values that will fill the db fields.
     """
-    loci_fields: List[Tuple[Union[int, str],...]] = field(default_factory=list)
+    loci_fields: List[Tuple[Union[int, str], ...]] = field(default_factory=list)
     scheme_members_fields: List[Tuple[Union[str, int], ...]] = field(default_factory=list)
-    client_dbase_loci_fields: List[Tuple[Union[str, int],...]] = field(default_factory=list)
+    client_dbase_loci_fields: List[Tuple[Union[str, int], ...]] = field(default_factory=list)
     isolates_loci_fields: List[Tuple[Union[str, int], ...]] = field(default_factory=list)
     sequences_fields: List[Tuple[Union[str, int], ...]] = field(default_factory=list)
+
 
 class GeneDetectionProfilesBatchInserter:
     """
     This class opens connections to seqdef and isolates DB and performs the insertion of gene detection schemes by batch
     """
+
     def __init__(self, species: str) -> None:
         """
         open database connections, one to isolates and the other to the seqdef db.
         :param species: commonly used bioit species name: either genus or specific like stec
         :return: None
         """
-        self.isolates_db_connection = DatabaseConnection(species,db_type='isolates', autocommit=False)
-        self.seqdef_db_connection = DatabaseConnection(species,db_type='seqdef', autocommit=False)
+        self.isolates_db_connection = DatabaseConnection(species, db_type='isolates', autocommit=False)
+        self.seqdef_db_connection = DatabaseConnection(species, db_type='seqdef', autocommit=False)
 
     def insert(self, batch_data: GeneDetectionProfilesBatchData) -> None:
         """
@@ -50,9 +53,10 @@ class GeneDetectionProfilesBatchInserter:
         except Exception as e:
             self.seqdef_db_connection.connection.rollback()
             self.isolates_db_connection.connection.rollback()
-            send_email( f'The followind error was raised during the insertion of gene detection scheme elements by batch: {e.args[0]}', f'GeneDetectionProfilesBatchInserter failed on {socket.gethostname()}')
+            send_email(
+                f'The followind error was raised during the insertion of gene detection scheme elements by batch: {e.args[0]}',
+                f'GeneDetectionProfilesBatchInserter failed on {socket.gethostname()}')
             raise Exception
-
 
     def insert_multiple_loci_seqdef(self, data: List) -> None:
         """
@@ -61,8 +65,10 @@ class GeneDetectionProfilesBatchInserter:
         :return: None
         """
         cur = self.seqdef_db_connection.cursor
-        args_str = ','.join(cur.mogrify('(%s,%s,%s,%s,%s,%s,%s,%s)',row).decode("utf-8") for row in data)
-        cur.execute("INSERT INTO {table} (id, data_type, allele_id_format, length_varies, coding_sequence, curator, date_entered, datestamp) VALUES ".format(table='loci') + args_str)
+        args_str = ','.join(cur.mogrify('(%s,%s,%s,%s,%s,%s,%s,%s)', row).decode("utf-8") for row in data)
+        cur.execute(
+            "INSERT INTO {table} (id, data_type, allele_id_format, length_varies, coding_sequence, curator, date_entered, datestamp) VALUES ".format(
+                table='loci') + args_str)
 
     @staticmethod
     def insert_multiple_scheme_members(cur: psycopg2.extensions.cursor, data: List) -> None:
@@ -81,7 +87,7 @@ class GeneDetectionProfilesBatchInserter:
         :param data: list of values to fill the sql insert statements
         :return: None
         """
-        cur=self.seqdef_db_connection.cursor
+        cur = self.seqdef_db_connection.cursor
         args_str = ','.join(cur.mogrify('(%s,%s,%s,%s)', row).decode("utf-8") for row in data)
         cur.execute("INSERT INTO {table} (client_dbase_id, locus, curator, datestamp) VALUES ".format(table='client_dbase_loci')+args_str)
 
@@ -92,8 +98,11 @@ class GeneDetectionProfilesBatchInserter:
         :return: None
         """
         cur = self.isolates_db_connection.cursor
-        args_str = ','.join(cur.mogrify('(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', row).decode("utf-8") for row in data)
-        cur.execute("INSERT INTO {table} (id, data_type, allele_id_format, length_varies, coding_sequence, dbase_name, dbase_id, url, isolate_display, main_display, query_field, analysis, submission_template, curator, date_entered, datestamp) VALUES ".format(table='loci') + args_str)
+        args_str = ','.join(
+            cur.mogrify('(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', row).decode("utf-8") for row in data)
+        cur.execute(
+            "INSERT INTO {table} (id, data_type, allele_id_format, length_varies, coding_sequence, dbase_name, dbase_id, url, isolate_display, main_display, query_field, analysis, submission_template, curator, date_entered, datestamp) VALUES ".format(
+                table='loci') + args_str)
 
     def insert_multiple_sequences(self, data: List) -> None:
         """
