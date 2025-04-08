a70d56a7e2291c69102b6b03cb3e76f18e2278e7
kamilis <kato.milis@sciensano.be>
2025-04-08 09:49:05 +0200

    Added new params type to execute_query (databaseconnection)

--- bioit_bigsdb_scripts/components/psql/databaseconnection.py
+++ bioit_bigsdb_scripts/components/psql/databaseconnection.py
@@ -45,7 +45,7 @@ class DatabaseConnection:
         self._cursor: psycopg2.extensions.cursor = self._connection.cursor()
         self.name = self._cursor.name
 
-    def execute_query(self, query: str, params: Union[Tuple[Union[str, int, Tuple[str]]], List[Union[str, int]]]) \
+    def execute_query(self, query: str, params: Union[Tuple[Union[str, int, Tuple[str]]], List[Union[str, int]], Tuple[str, str, str, str, str]]) \
             -> Optional[List[Optional[Tuple[Any]]]]:
         """
         Executes a sql query using psycopg2 sanitazation
