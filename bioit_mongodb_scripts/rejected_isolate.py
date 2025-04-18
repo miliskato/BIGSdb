bbf4d29fb2360fd6111aa6d2630cbc38f26bc5fc
kamilis <kato.milis@sciensano.be>
2025-04-18 16:14:55 +0200

    Code review validation goodqc + exporting JSON reports (part 2)

--- bioit_mongodb_scripts/rejected_isolate.py
+++ bioit_mongodb_scripts/rejected_isolate.py
@@ -117,7 +117,7 @@ class RejectedIsolate:
         """
         json_path_remote = Path(self._rejected_isolate_document['report_directory']) / 'report.json'
         with tempfile.NamedTemporaryFile(dir=self._mongo_config_data.get('temp_dir')) as temp_json:
-            scp_command = f"scp -o StrictHostKeyChecking=no -i /home/bigsdb/.ssh/.id_rsa_reportsapi bigsdb@{self._mongo_config_data.get('azure_reportsapi_ip')}:{json_path_remote} {temp_json.name})"
+            scp_command = f"scp -o StrictHostKeyChecking=no -i /home/bigsdb/.ssh/.id_rsa_reportsapi bigsdb@{self._mongo_config_data.get('azure_reportsapi_ip')}:{json_path_remote} {temp_json.name}"
             scp_cmd = Command(scp_command)
             scp_cmd.run(Path(self._mongo_config_data.get('temp_dir')))
             if scp_cmd.returncode != 0:
