705f9cef150af1df47031ba536765700f0a23b88
kamilis <kato.milis@sciensano.be>
2025-05-16 10:39:39 +0200

    Bugfixes psycopg3 instead of psycopg2

--- bioit_bigsdb_scripts/components/psql/databaseconnection.py
+++ bioit_bigsdb_scripts/components/psql/databaseconnection.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
-from typing import Any, List, Optional, Tuple, Union, Literal
+from types import TracebackType
+from typing import Any, List, Optional, Tuple, Union, Literal, Type
 
 import psycopg
 
