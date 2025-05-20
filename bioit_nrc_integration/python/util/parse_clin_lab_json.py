6b08540acd6fb71a620fd0bb8af81465071ed276
mikelchtermans <michael.kelchtermans@sciensano.be>
2025-05-20 09:55:31 +0200

    Address code review comments ASG on nrc integration

--- bioit_nrc_integration/python/util/parse_clin_lab_json.py
+++ bioit_nrc_integration/python/util/parse_clin_lab_json.py
@@ -127,18 +127,13 @@ class ParseClinLabJson(object, metaclass=abc.ABCMeta):
         # be stored in the mic_resistances_list, ordered alphabetically and concatenated with spaces in between.
         mic_resistances_list: list[str] = []
         if labtest_list_of_result_dicts:
+            labtest_code_combinations = self._translation_codes['code_lists']['TX_TTL_LAB_TEST_combinations']
             for labtest_result_dict in labtest_list_of_result_dicts:
-                labtest_code_combinations = self._translation_codes['code_lists']['TX_TTL_LAB_TEST_combinations']
                 labtest_code_combination = next((
                     labtest_code_combination for labtest_code_combination in labtest_code_combinations if
-                    # CD_LAB_TEST_METH is mandatory I believe
-                    labtest_code_combination['CD_LAB_TEST_METH'] == self.___get_value_by_capitalization_agnostic_key(
-                        labtest_result_dict, 'CD_LAB_TEST_METH')
-                    # CD_LAB_TEST_CODE seems to be optional; if null in code list then .get results in False
-                    # e.g. for serotyping this field does not seem to be filled because there are no subtests
-                    and (not labtest_code_combination.get('CD_LAB_TEST_CODE') or labtest_code_combination.get(
-                        'CD_LAB_TEST_CODE') == self.___get_value_by_capitalization_agnostic_key(
-                        labtest_result_dict, 'CD_LAB_TEST_CODE'))))
+                    labtest_code_combination['CD_LAB_TEST_CODE'] == self.___get_value_by_capitalization_agnostic_key(
+                        labtest_result_dict, 'CD_LAB_TEST_CODE')))
+                translation = labtest_code_combination['translation']
 
                 if labtest_code_combination.get('code_list'):
                     # Even if the field's value supposedly needs to come from a code list, there can be exceptions
@@ -147,22 +142,21 @@ class ParseClinLabJson(object, metaclass=abc.ABCMeta):
                     code_value = self.___cast_as_int_if_int(self.___get_value_by_capitalization_agnostic_key(
                         labtest_result_dict, labtest_code_combination['value_field']))
                     if code_value:
-                        self._data_translated[labtest_code_combination['translation']] = \
-                            self._translation_codes['code_lists'][labtest_code_combination['code_list']][code_value]
-                        if labtest_code_combination['translation'].startswith('mic_') and \
-                                labtest_code_combination['translation'].endswith('_I') and \
-                                self._data_translated[labtest_code_combination['translation']] == 'Resistant':
-                            mic_resistances_list.append((labtest_code_combination['translation'].split('_'))[1])
+                        self._data_translated[translation] = self._translation_codes['code_lists'][labtest_code_combination['code_list']][code_value]
+                        if translation.startswith('mic_') and \
+                                translation.endswith('_I') and \
+                                self._data_translated[translation] == 'Resistant':
+                            mic_resistances_list.append((translation.split('_'))[1])
                     else:
-                        self._data_translated[labtest_code_combination['translation']] = \
+                        self._data_translated[translation] = \
                             self.___get_value_by_capitalization_agnostic_key(labtest_result_dict, 'TX_LAB_TEST_RSLT_TXT')
                 else:
-                    self._data_translated[labtest_code_combination['translation']] = \
+                    self._data_translated[translation] = \
                         self.___get_value_by_capitalization_agnostic_key(
                             labtest_result_dict, labtest_code_combination['value_field'])
-        if mic_resistances_list:
-            mic_resistances_list.sort()
-            self._data_translated['mic_resistances'] = ' '.join([resistance for resistance in mic_resistances_list])
+            if mic_resistances_list:
+                mic_resistances_list.sort()
+                self._data_translated['mic_resistances'] = ' '.join([resistance for resistance in mic_resistances_list])
 
     def __parse_complex_country_field(self) -> None:
         """
