a8779b131bc89ab9ff26f20b7e2b1d8f77c93eb7
angori <ann-stephan.gori@sciensano.be>
2025-05-28 17:46:28 +0200

    fix bug for BactMet insertion in BIGSdb

--- bioit_bigsdb_scripts/components/maininserter.py
+++ bioit_bigsdb_scripts/components/maininserter.py
@@ -157,4 +157,4 @@ class MainInserter(JsonSuperClass):
                     html_mutation = lre_mutation_table_builder.build()
                     self._isolates_eavt_psql_tbl.insert_eav_isolate((self._isolatename, 'LRE-Finder_mutations', html_mutation))
             if 'bacmet' in self._json_report_dict and self._json_report_dict['bacmet']['bacmet_genes'] != '':
-                self._isolates_eavt_psql_tbl.insert_eav_isolate((self._isolatename, 'BactMet_genes', self._json_report_dict['bacmet']['bacmet_gene']))
+                self._isolates_eavt_psql_tbl.insert_eav_isolate((self._isolatename, 'BactMet_genes', self._json_report_dict['bacmet']['bacmet_genes']))
