998f4fc3027a673fc1ad9ba12ff5a57c021ecb6a
angori <ann-stephan.gori@sciensano.be>
2025-06-23 16:41:43 +0200

    add nominative metadata for MTB

---
+++ sql/bigsdb_isolates_v/bigsdb_mycobacterium_isolates/db/migrations/20250623000000_mycobacterium_specific_metadata.sql
@@ -0,0 +1,29 @@
+-- migrate:up
+ALTER TABLE isolates
+    ADD COLUMN patient_first_name text,
+    ADD COLUMN patient_last_name text,
+    ADD COLUMN physician_name text,
+    ADD COLUMN sampling_date date,
+    ADD COLUMN reception_date date,
+    ADD COLUMN positive_culture_date date,
+    ADD COLUMN identification_BK text,
+    ADD COLUMN identification_NTM text,
+    ADD COLUMN isoniazid_0_1ug text,
+    ADD COLUMN rifampicin_1ug text,
+    ADD COLUMN ethambutol_5ug text,
+    ADD COLUMN pyrazinamid_100ug text;
+
+-- migrate: down
+ALTER TABLE isolates
+    DROP COLUMN patient_first_name,
+    DROP COLUMN patient_last_name,
+    DROP COLUMN physician_name,
+    DROP COLUMN sampling_date,
+    DROP COLUMN reception_date,
+    DROP COLUMN positive_culture_date,
+    DROP COLUMN identification_BK,
+    DROP COLUMN identification_NTM,
+    DROP COLUMN isoniazid_0_1ug,
+    DROP COLUMN rifampicin_1ug,
+    DROP COLUMN ethambutol_5ug,
+    DROP COLUMN pyrazinamid_100ug;
