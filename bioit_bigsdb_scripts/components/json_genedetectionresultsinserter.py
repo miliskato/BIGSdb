0d522749d47133491b5126caf7c7337d69dad274
kamilis <kato.milis@sciensano.be>
2025-06-25 09:04:57 +0200

    Adaptation amrfinder insertion after update of tool and database + inclusion of psycopg in requirements mongo

--- bioit_bigsdb_scripts/components/json_genedetectionresultsinserter.py
+++ bioit_bigsdb_scripts/components/json_genedetectionresultsinserter.py
@@ -88,8 +88,8 @@ class JsonGeneDetectionResultsInserter(JsonSuperClass):
                 elif scheme == 'amrfinder':
                     amrfinder_table_builder = HtmlAmrTableBuilder(report_url)
                     for hit in self._json_report_dict[scheme]['amr_genes_hits']:
-                        identity = f'{round(float(hit["% Identity to reference sequence"]), 2)}'
-                        coverage = f'{round(float(hit["% Coverage of reference sequence"]), 2)}'
+                        identity = f'{round(float(hit["% Identity to reference"]), 2)}'
+                        coverage = f'{round(float(hit["% Coverage of reference"]), 2)}'
                         amrfinder_table_builder.add_hit(hit['Subclass'], hit['Gene symbol'], identity, coverage)
                     html = amrfinder_table_builder.build()
                 elif not scheme.endswith('vfdb_core') and not scheme.endswith('virulencefinder'):
