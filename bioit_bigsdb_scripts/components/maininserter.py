8eb53fe6344d3c4391c12e4ef9f22ba6a5099455
angori <ann-stephan.gori@sciensano.be>
2025-07-02 17:40:13 +0200

    remove unused functions

--- bioit_bigsdb_scripts/components/maininserter.py
+++ bioit_bigsdb_scripts/components/maininserter.py
@@ -6,7 +6,6 @@ from typing import Any, Dict
 from bioit_mongodb_scripts.model.json_model import JsonReportDict
 from .json_superclass import JsonSuperClass
 from .psql import TblEavInt, TblEavText, TblEavTextHidden, TblHistory, TblIsolates
-from ..utils.html_tbl_templates import HtmlLreFinderGenesTableBuilder, HtmlLreFinderMutationsTableBuilder
 from ..utils.url_helper import UrlHelper
 
 
