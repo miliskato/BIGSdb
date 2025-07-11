26fa3002e8cdd3ba03032cc498abda81ce6a3bc2
mikelchtermans <michael.kelchtermans@sciensano.be>
2025-07-11 15:15:50 +0200

    Neisseria clin & lab dcd integration first version

--- bioit_nrc_integration/python/util/get_clin_lab_json_parser.py
+++ bioit_nrc_integration/python/util/get_clin_lab_json_parser.py
@@ -4,6 +4,7 @@ from .parse_clin_lab_json import ParseClinLabJson
 from .parse_clin_lab_json_influenza import ParseClinLabJsonInfluenza
 from .parse_clin_lab_json_listeria import ParseClinLabJsonListeria
 from .parse_clin_lab_json_mycobacterium import ParseClinLabJsonMycobacterium
+from .parse_clin_lab_json_neisseria import ParseClinLabJsonNeisseria
 from .parse_clin_lab_json_salmonella import ParseClinLabJsonSalmonella
 
 
@@ -15,7 +16,7 @@ def get_clin_lab_json_parser(species: str) -> Type[ParseClinLabJson]:
     :return: ParseClinLabJson
     """
     for parser in [ParseClinLabJsonInfluenza, ParseClinLabJsonListeria, ParseClinLabJsonMycobacterium,
-                   ParseClinLabJsonSalmonella]:
+                   ParseClinLabJsonNeisseria, ParseClinLabJsonSalmonella]:
         if species == parser.TARGET_SPECIES:
             return parser
     return ParseClinLabJson
