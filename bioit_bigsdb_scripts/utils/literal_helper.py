ddade40233e75e873e38559f2351253122e69bf4
angori <ann-stephan.gori@sciensano.be>
2025-07-11 13:59:59 +0200

    docstring

--- bioit_bigsdb_scripts/utils/literal_helper.py
+++ bioit_bigsdb_scripts/utils/literal_helper.py
@@ -1,7 +1,13 @@
 from typing import Literal, get_args, get_origin
 
 
-def validate_literal(value, literal_type):
+def validate_literal(value, literal_type) -> None:
+    """
+    small function to validate that the value used is part of a Literal
+    :param value: value to validate
+    :param literal_type: the Literal type
+    :return: None
+    """
     if not get_origin(literal_type) is Literal:
         raise TypeError(f"{literal_type} is not a Literal type")
     if value in get_args(literal_type):
