26feb2c0c11803807bee7cfbc812ca66b9a06aa3
angori <ann-stephan.gori@sciensano.be>
2025-07-16 12:00:34 +0200

    adapt to new version of the mongo db report

--- bioit_bigsdb_scripts/components/json_genedetectionresultsinserter.py
+++ bioit_bigsdb_scripts/components/json_genedetectionresultsinserter.py
@@ -60,7 +60,7 @@ class JsonGeneDetectionResultsInserter(JsonSuperClass):
             elif scheme == 'amrfinder':
                 listofhits: List = self._json_report_dict[scheme]['amr_genes_hits']
             elif scheme == 'lrefinder':
-                if isinstance(self._json_report_dict[scheme]['lrefinder_genes'], str):
+                if self._json_report_dict[scheme]['lrefinder_genes'][0].get('Gene') == 'Unknown':
                     return
                 listofhits: List = self._json_report_dict['lrefinder']['lrefinder_genes']
             else:
@@ -93,9 +93,9 @@ class JsonGeneDetectionResultsInserter(JsonSuperClass):
                 elif scheme == 'amrfinder':
                     amrfinder_table_builder = HtmlAmrTableBuilder(report_url)
                     for hit in self._json_report_dict[scheme]['amr_genes_hits']:
-                        identity = f'{round(float(hit["% Identity to reference sequence"]), 2)}'
-                        coverage = f'{round(float(hit["% Coverage of reference sequence"]), 2)}'
-                        amrfinder_table_builder.add_hit(hit['Subclass'], hit['Gene symbol'], identity, coverage)
+                        identity = f'{round(float(hit["% Identity to reference"]), 2)}'
+                        coverage = f'{round(float(hit["% Coverage of reference"]), 2)}'
+                        amrfinder_table_builder.add_hit(hit['Subclass'], hit['Element symbol'], identity, coverage)
                     html = amrfinder_table_builder.build()
 
                 elif scheme == 'lrefinder':
