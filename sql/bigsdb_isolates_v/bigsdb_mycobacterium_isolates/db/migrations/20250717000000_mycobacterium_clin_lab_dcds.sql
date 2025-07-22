df761dcc319c46259403a020a75af51f607f7b9e
mikelchtermans <michael.kelchtermans@sciensano.be>
2025-07-22 11:30:39 +0200

    Remove positive_culture_date field from mycobacterium because not present in DCD, agreed by Florian

--- sql/bigsdb_isolates_v/bigsdb_mycobacterium_isolates/db/migrations/20250717000000_mycobacterium_clin_lab_dcds.sql
+++ sql/bigsdb_isolates_v/bigsdb_mycobacterium_isolates/db/migrations/20250717000000_mycobacterium_clin_lab_dcds.sql
@@ -3,11 +3,13 @@ ALTER TABLE isolates
     ADD COLUMN under_treatment text,
     ADD COLUMN tuberculosis_antecedents text,
     ADD COLUMN tuberculosis_site text,
-    ADD COLUMN tuberculosis_suspicion text;
+    ADD COLUMN tuberculosis_suspicion text,
+    DROP COLUMN positive_culture_date;
 
 -- migrate:down
 ALTER TABLE isolates
     DROP COLUMN under_treatment,
     DROP COLUMN tuberculosis_antecedents,
     DROP COLUMN tuberculosis_site,
-    DROP COLUMN tuberculosis_suspicion;
+    DROP COLUMN tuberculosis_suspicion,
+    ADD COLUMN positive_culture_date date;
