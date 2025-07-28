ee85f1a08592f79e8d0a2a1bcb319408fd6c70e1
angori <ann-stephan.gori@sciensano.be>
2025-07-28 16:12:53 +0200

    cover Florian's request to get only one summary field for the PCR subtype or lineage

--- sql/bigsdb_isolates_v/bigsdb_influenza_isolates/db/migrations/20250514000000_influenza_specific_metadata.sql
+++ sql/bigsdb_isolates_v/bigsdb_influenza_isolates/db/migrations/20250514000000_influenza_specific_metadata.sql
@@ -1,9 +1,18 @@
 -- migrate:up
 ALTER TABLE isolates
     ADD COLUMN FluA_SubtypeHAPCR text,
-    ADD COLUMN FluA_SubtypeNAPCR text;
+    ADD COLUMN FluA_SubtypeNAPCR text,
+    ADD COLUMN Flu_subtype_or_lineagePCR text;
+ALTER TABLE isolates
+    DROP COLUMN FluA_subtypePCR,
+    DROP COLUMN FluB_lineagePCR;
+
 
 -- migrate:down
 ALTER TABLE isolates
     DROP COLUMN FluA_SubtypeHAPCR,
-    DROP COLUMN FluA_SubtypeNAPCR;
+    DROP COLUMN FluA_SubtypeNAPCR,
+    DROP COLUMN Flu_subtype_or_lineagePCR;
+ALTER TABLE isolates
+    ADD COLUMN FluA_subtypePCR text,
+    ADD COLUMN FluB_lineagePCR text;
