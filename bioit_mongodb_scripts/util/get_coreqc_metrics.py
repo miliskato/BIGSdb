330e95414d7c284db5eb0ea8c161b03561a7af38
kamilis <kato.milis@sciensano.be>
2025-09-11 16:32:42 +0200

    Adapt ont contamination warning threshold for Salmonella

--- bioit_mongodb_scripts/util/get_coreqc_metrics.py
+++ bioit_mongodb_scripts/util/get_coreqc_metrics.py
@@ -53,7 +53,7 @@ class GetCoreQCMetrics:
                     sample_coreqc_metrics.pop(key)
         elif self._reads_input_type != 'illumina':
             self.__set_global_ont_coverage_thresholds(sample_coreqc_metrics)
-            self.__set_ont_contamination_failure_threshold(sample_coreqc_metrics)
+            self.__set_ont_contamination_thresholds(sample_coreqc_metrics)
 
         self.__convert_iterations_to_separate_keys(sample_coreqc_metrics)
 
@@ -116,15 +116,16 @@ class GetCoreQCMetrics:
                 metric_info['threshold_warn'] = 50.0
                 metric_info['threshold_fail'] = 30.0
 
-    def __set_ont_contamination_failure_threshold(self, sample_coreqc_metrics: dict[str, Any]) -> None:
+    def __set_ont_contamination_thresholds(self, sample_coreqc_metrics: dict[str, Any]) -> None:
         """
         For Salmonella (later probably also additional species), the ont contamination failure threshold is 10% instead
-        of 5%.
+        of 5% and the warning threshold is 5% instead of 1%.
         :param sample_coreqc_metrics: core quality metrics and thresholds for current sample, to be modified in place.
         :return: None
         """
         for key, metric_info in sample_coreqc_metrics.items():
             if metric_info.get('ont_contamination') and self._species == 'salmonella':
+                metric_info['threshold_warn'] = 5.0
                 metric_info['threshold_fail'] = 10.0
 
     @staticmethod
