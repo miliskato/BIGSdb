64b802aa4dd6b9aae1f8b85286ed6a3de825e100
mikelchtermans <michael.kelchtermans@sciensano.be>
2025-09-23 15:12:52 +0200

    NRC integration neisseria output genomic DCD

--- bioit_nrc_integration/python/send_genomic_to_ODS.py
+++ bioit_nrc_integration/python/send_genomic_to_ODS.py
@@ -36,7 +36,7 @@ class SendGenomicToODS:
         self._document = document
         self._species = species
 
-        # get HD ODS dictionaries to be able to translate to useable text
+        # get HD ODS dictionaries to be able to translate to usable text
         with CODES_GENOMIC_ODS.open('r') as handle:
             self._translation_codes = yaml.safe_load(handle)
 
@@ -54,7 +54,7 @@ class SendGenomicToODS:
         data_dict = {}
         for variable, variable_info in self._translation_codes['common'].items():
             data_dict[variable] = access_value_in_dict_using_list_as_dictpath(variable_info['dict_path'],
-                                                                              self._document)
+                                                                              dict(self._document))
         # although the DT_PIPELINE_ANAL is common, it can not be processed regularly using
         # the previous function because it needs to be converted
         data_dict['DT_PIPELINE_ANAL'] = datetime.strptime(self._document['results']['analysis_date'],
@@ -62,8 +62,11 @@ class SendGenomicToODS:
 
         if self._translation_codes.get(self._species):
             for variable, variable_info in self._translation_codes[self._species].items():
+                if variable_info.get('custom'):
+                    self.__parse_custom_results(variable_info, data_dict)
+                    continue
                 data_dict[variable] = access_value_in_dict_using_list_as_dictpath(variable_info['dict_path'],
-                                                                                  self._document)
+                                                                                  dict(self._document))
                 if variable_info.get('code_list'):
                     data_dict[variable] = self._translation_codes['code_lists'][
                         variable_info['code_list']][data_dict[variable]]
@@ -85,3 +88,41 @@ class SendGenomicToODS:
             if match:
                 data_dict['TX_GENTPE_SUBTPE_HEMAG'] = f"H{match.group(1)}"
                 data_dict['TX_GENTPE_SUBTPE_NEURAM'] = f"N{match.group(2)}"
+
+    def __parse_custom_results(self, variable_info, data_dict: dict[str, Any]) -> None:
+        """
+        Wrapper function to parse genomic results in a custom manner.
+        :param variable_info: the current variable's info
+        :param data_dict: the data dictionary to send to the ODS, to be modified in place.
+        :return: None
+        """
+        if variable_info['custom'] == 'resfinder':
+            self.__parse_custom_results_resfinder4(variable_info, data_dict)
+        else:
+            raise NotImplementedError(f"Custom parsing method {variable_info['custom']} not implemented.")
+
+        pass
+
+    def __parse_custom_results_resfinder4(self, variable_info, data_dict: dict[str, Any]) -> None:
+        """
+        Parses the results of ResFinder4 in a custom manner. The antibiotic phenotypes are stored in a 'Phenotype' field
+        in dictionaries in a list. The Phenotype field can store one or multiple comma separated values, but the
+        formatting of the comma separation is not always consistent. This code tries to clean up the inconsistencies,
+        and then for every unique antibiotic, adds a 'Resistant' value to the output data dictionary.
+        :param variable_info: the current variable's info
+        :param data_dict: the data dictionary to send to the ODS, to be modified in place.
+        :return: None
+        """
+        hits: list[dict[str, Any]] = access_value_in_dict_using_list_as_dictpath(variable_info['dict_path'], dict(self._document))
+        phenotypes = set([hit['Phenotype'] for hit in hits])
+        antibiotics = []
+        for phenotype in phenotypes:
+            antibiotics.extend(phenotype.split(','))
+        antibiotics_reformatted = set([antibiotic.lower().replace(' ') for antibiotic in antibiotics])
+        for antibiotic in antibiotics_reformatted:
+            antibiotic_code = self._translation_codes['custom']['resfinder4'].get(antibiotic)
+            # Ignore potential new antibiotics that are not in DCD
+            if antibiotic_code:
+                # Add Resistant info, don't add Susceptible info because ResFinder only gives info about resistances and
+                # not about susceptibilities.
+                data_dict[antibiotic_code] = self._translation_codes['code_lists']['SRI']['Resistant']
