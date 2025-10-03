2143a17928e7c8219460a199bf533b34059bda0c
angori <ann-stephan.gori@sciensano.be>
2025-10-03 15:07:07 +0200

    bugfix

--- sql/bigsdb_isolates_v/common/db/migrations/20250926131300_add_isolate_fields.sql
+++ sql/bigsdb_isolates_v/common/db/migrations/20250926131300_add_isolate_fields.sql
@@ -1,11 +1,11 @@
 -- migrate:up
 ALTER TABLE isolates
     ADD COLUMN html text,
-    ADD COLUMN pipeline text;
+    ADD COLUMN pipeline text,
     ADD COLUMN assembly text;
 
 -- migrate:down
 ALTER TABLE isolates
     DROP COLUMN html,
-    DROP COLUMN pipeline;
+    DROP COLUMN pipeline,
     DROP COLUMN assembly;
