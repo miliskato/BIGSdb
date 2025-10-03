79d661ebbc204b76e2ab76a2af330c3e294088da
angori <ann-stephan.gori@sciensano.be>
2025-10-03 12:10:31 +0200

    add assembly

--- sql/bigsdb_isolates_v/common/db/migrations/20250926131300_add_isolate_fields.sql
+++ sql/bigsdb_isolates_v/common/db/migrations/20250926131300_add_isolate_fields.sql
@@ -2,8 +2,10 @@
 ALTER TABLE isolates
     ADD COLUMN html text,
     ADD COLUMN pipeline text;
+    ADD COLUMN assembly text;
 
 -- migrate:down
 ALTER TABLE isolates
     DROP COLUMN html,
     DROP COLUMN pipeline;
+    DROP COLUMN assembly;
