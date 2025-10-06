231559b74ac38718ff4df5aaafc5ba55a1b5eb6f
angori <ann-stephan.gori@sciensano.be>
2025-10-06 12:58:47 +0200

    reorganize files for package factories.main_results_factory in the project

---
+++ factories/main_results_factory/__init__.py
@@ -0,0 +1,8 @@
+from .summary_results_builder_factory import SummaryResultsBuilderFactory
+from .builders import SummaryResultsBuilder
+from .builders import GenericSummaryResultsBuilder
+from .builders import NeisseriaSummaryResultsBuilder
+from .builders import ListeriaSummaryResultsBuilder
+from .data_typing import SequenceTypingData, NeisseriaSequenceTypingData, ListeriaSequenceTypingData
+from .data_typing import SerotypingData, NeisseriaSerotypingData
+from .utils import JsonMakerSummaryResults
