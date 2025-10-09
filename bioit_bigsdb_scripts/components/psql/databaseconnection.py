a8ca0371e78785d42f592b9cd3424144daf570f7
angori <ann-stephan.gori@sciensano.be>
2025-10-09 12:39:25 +0200

    fix other typing issues found in psql_tbl_mapping_table.py

--- bioit_bigsdb_scripts/components/psql/databaseconnection.py
+++ bioit_bigsdb_scripts/components/psql/databaseconnection.py
@@ -5,10 +5,15 @@ from typing import Any, List, Optional, Tuple, Union, Literal, Type
 
 import psycopg
 
+from bioit_bigsdb_scripts.utils.literal_helper import validate_literal
+
 PYTHONPATH = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(PYTHONPATH))
 
 from bioit_mongodb_scripts.util.python_utility_functions import get_bigsdb_config_data
+
+DbLiteral = Literal['seqdef', 'isolates', 'jobs']
+DbValue = Union[DbLiteral, str]
 
 
 class DatabaseConnection:
@@ -16,7 +21,8 @@ class DatabaseConnection:
     Class containing function to open database connections to BIGSdb. To be used with a context manager, which
     automatically closes the connection and commits or rolls back
     """
-    def __init__(self, species: str, db_type: Literal['seqdef', 'isolates', 'jobs'], autocommit: bool = True) -> None:
+
+    def __init__(self, species: str, db_type: DbValue, autocommit: bool = True) -> None:
         """
         Initialises a database connection.
         :param species: commonly used bioit species name: either genus or specific like stec
@@ -24,6 +30,7 @@ class DatabaseConnection:
         :param autocommit: if True every operation is committed immediately after execution
         :return: None
         """
+        validate_literal(db_type, DbLiteral)
         self._db_type = db_type
         # Read the global config
         bigsdb_config_data = get_bigsdb_config_data()
@@ -42,7 +49,7 @@ class DatabaseConnection:
         except Exception:
             raise RuntimeError(f"Could not connect to {species}'s databases")
 
-    def execute_query(self, query: str, params: Union[Tuple[Union[str, int, Tuple[str]]], List[Union[str, int]], Tuple[str, str, float]]) \
+    def execute_query(self, query: str, params: Union[Tuple[Union[str, int, Tuple[str]]], List[Union[str, int]], Tuple[str, str, float], Tuple[str, str, str]]) \
             -> Optional[List[Optional[Tuple[Any]]]]:
         """
         Executes a sql query using psycopg sanitization
